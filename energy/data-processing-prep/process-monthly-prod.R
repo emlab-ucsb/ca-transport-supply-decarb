41682379332446d78b0b5286117496d7e905e9d6
traceybit <traceymangin@gmail.com>
2021-03-11 15:22:36 -0800

    save pre-processed monthly well production that 1) pads field codes; 2) adds field name; 3) excludes gas fields; 4) reassign old wilmington abd to wilmington (field code and field name); 5) adds column "month_year", and renames doc field names and field code columns.

---
+++ energy/data-processing-prep/process-monthly-prod.R
@@ -0,0 +1,60 @@
+## Tracey Mangin
+## March 10, 2021
+## process well production, save for later use through out
+
+# ------------------------------------------- INPUTS -----------------------------------
+
+data_dir     <- "/Volumes/GoogleDrive/Shared\ drives/emlab/projects/current-projects/calepa-cn/data/stocks-flows/processed/"
+mprod_fil    <- "well_prod_m.rds"
+wells_19_fil <- "wells_19.csv"
+
+# ------------------------------------------- MAIN -----------------------------------
+
+# load libraries -------- 
+
+library(data.table)  
+library(tidyverse)
+library(zoo)
+
+# load data ------
+
+wells_19 <- fread(paste0(data_dir, wells_19_fil))
+wells_19[, api_ten_digit := substr(API, 1, 10)]
+wells_19[, FieldCode := paste0("00", FieldCode)][, FieldCode := str_sub(FieldCode, start= -3)]
+
+well_prod <- read_rds(paste0(data_dir, "well_prod_m.rds"))
+
+setDT(well_prod)
+
+well_prod[, ':=' (api_ten_digit = substr(APINumber, 1, 10),
+                  FieldCode = paste0("00", FieldCode))][, FieldCode := str_sub(FieldCode, start= -3)]
+
+well_prod[, month_year :=  as.Date(as.yearmon(paste(year, month, sep = "-")))]
+
+## make df of field codes and field names to join with well prod
+fieldcodes <- unique(wells_19[, .(FieldCode, FieldName)])
+  
+## join to well prod
+well_prod <- merge(well_prod, fieldcodes)
+well_prod[, ':=' (FieldCode = fifelse(FieldCode == "848", "849", FieldCode),
+                  FieldName = fifelse(FieldName == "Old Wilmington (ABD)", "Wilmington", FieldName))]
+
+setnames(well_prod, c("FieldCode", "FieldName"), c('doc_field_code', "doc_fieldname"))
+
+## remove gas fields
+well_prod[, gas_field := str_detect(doc_fieldname, "Gas")][, gas_field := ifelse(gas_field == TRUE, 1, 0)]
+
+## check
+field_types <- unique(well_prod[, .(doc_fieldname, doc_field_code, gas_field)])
+
+## filter out gas fields
+well_prod <- well_prod[gas_field == 0]
+well_prod[, gas_field := NULL]
+
+setcolorder(well_prod, c("ReportType", "APINumber", "api_ten_digit", "doc_field_code", "doc_fieldname", "county", 
+                         "county_name", "AreaCode", "PoolCode", "WellTypeCode", "well_type_name", "ProductionReportDate", 
+                         "year", "month", "month_year", "ProductionStatus", "CasingPressure", "TubingPressure", 
+                         "BTUofGasProduced", "MethodOfOperation", "APIGravityofOil", "WaterDisposition", "OilorCondensateProduced", "DaysProducing",          
+                         "GasProduced", "WaterProduced", "ReportedOrEstimated"))
+
+fwrite(well_prod, paste0(data_dir, 'well_prod_m_processed.csv'))
