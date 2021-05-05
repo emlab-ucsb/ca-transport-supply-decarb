5d0bece874db657bac378d7f050f80ee4bfff17d
traceybit <traceymangin@gmail.com>
2021-05-04 18:14:43 -0700

    working on setback

--- energy/analysis/gen_well_setback_status.R
+++ energy/analysis/gen_well_setback_status.R
@@ -7,6 +7,7 @@
 
 # comment out and add your own machine's file path
 home <- "/Volumes/GoogleDrive/Shared drives"
+ft_path <- "emlab/projects/current-projects/calepa-cn/data/FracTracker/FracTrackerSetbackgdb-newest/FracTrackerSetbackgdb/FracTrackerSetbackdata.gdb"
 
 # load packages
 library(sf)
@@ -17,6 +18,10 @@ library(gdalUtilities)
 library(maps)
 library(mapview)
 
+# transform to NAD83(NSRS2007) / California Albers
+# units will be in meters
+ca_crs <- 3488
+
 ## quick CA
 ca <- st_as_sf(map("state", plot = FALSE, fill = TRUE)) %>%
   filter(ID == "california")
@@ -26,12 +31,188 @@ ca <- st_as_sf(map("state", plot = FALSE, fill = TRUE)) %>%
 # to get the names of layers in the shapefile
 layers <- sf::st_layers(dsn = file.path(home, "emlab/projects/current-projects/calepa-cn/data/FracTracker/FracTrackerSetbackgdb-newest/FracTrackerSetbackgdb/FracTrackerSetbackdata.gdb"))
 
-sr <- sf::st_read(dsn = file.path(home, "emlab/projects/current-projects/calepa-cn/data/FracTracker/FracTrackerSetbackgdb-newest/FracTrackerSetbackgdb/FracTrackerSetbackdata.gdb"), layer = "SetbackOutlines_SR_Dwellings_082220")
-sr <- sr  %>% st_transform(3488) 
-sr <- sf::st_cast(sr, "MULTIPOLYGON")
+## read in the SR layers
+layer_vec <- c("SetbackOutlines_SR_Dwellings_082220", "PlaygroundsinCities", "DayCareCenters", "reselderlyCare",
+               "CHHS_adultdayhealthcare_csv_Events", "CHHS_altbirthing_csv_Events", "CHHS_Dialysis_csv_Events", 
+               "CHHS_healthcare_facility_locations_csv_Events", "CHHS_intermedcarefac_csv_Events", 
+               "CHHS_PrimaryCareClinic_csv_Events", "CHHS_psychclinics_csv_Events",
+               "CHHS_rehabclinic_csv_Events", "CHHS_skillednursingfacs_csv_Events", "CHHS_surgicalclinic_csv_Events",
+               "CHHS_acutecarehospital_csv_Events_1", "CAAcuteCAreHostpitalslatlon_1", "PrivSchoolsCA_1", "SchoolPropCA_1",
+               "SchoolsCA_Sabins_1")
 
-# transform to NAD83(NSRS2007) / California Albers
-# units will be in meters
+
+## dwellings
+sr_dwellings <- sf::st_read(dsn = file.path(home, ft_path), layer = "SetbackOutlines_SR_Dwellings_082220")
+sr_dwellings <- sr_dwellings  %>% st_transform(ca_crs) 
+sr_dwellings <- sf::st_cast(sr_dwellings, "MULTIPOLYGON")
+sr_dwellings <- st_union(sr_dwellings)
+
+## playgrounds
+sr_pg <- sf::st_read(dsn = file.path(home, ft_path), layer = "PlaygroundsinCities") %>%
+  st_transform(ca_crs) %>%
+  dplyr::select(fac_type = FAC_TYPE)
+
+## day care centers
+sr_dc <- sf::st_read(dsn = file.path(home, ft_path), layer = "DayCareCenters") %>%
+  st_transform(ca_crs) %>%
+  mutate(fac_type = "child day care center") %>%
+  dplyr::select(fac_type)
+
+## res elderly care
+sr_ec <- sf::st_read(dsn = file.path(home, ft_path), layer = "reselderlyCare") %>%
+  st_transform(ca_crs) %>%
+  mutate(fac_type = "retirement community") %>%
+  dplyr::select(fac_type)
+
+## chhs adult day health care
+sr_adhc <- sf::st_read(dsn = file.path(home, ft_path), layer = "CHHS_adultdayhealthcare_csv_Events") %>%
+  st_transform(ca_crs)
+
+## alt birthing
+sr_ab <- sf::st_read(dsn = file.path(home, ft_path), layer = "CHHS_altbirthing_csv_Events") %>%
+  st_transform(ca_crs) %>%
+  mutate(fac_type = "alternative birthing center") %>%
+  dplyr::select(fac_type)
+
+## dialysis
+sr_d <- sf::st_read(dsn = file.path(home, ft_path), layer = "CHHS_Dialysis_csv_Events") %>%
+  st_transform(ca_crs) %>%
+  mutate(fac_type = "dialysis clinic") %>%
+  dplyr::select(fac_type)
+
+## health care
+sr_hc <- sf::st_read(dsn = file.path(home, ft_path), layer = "CHHS_healthcare_facility_locations_csv_Events") %>%
+  st_transform(ca_crs) %>%
+  mutate(fac_type = "skilled nursing facility") %>%
+  dplyr::select(fac_type)
+
+## med care
+sr_imc <- sf::st_read(dsn = file.path(home, ft_path), layer = "CHHS_intermedcarefac_csv_Events") %>%
+  st_transform(ca_crs) %>%
+  mutate(fac_type = "hospice") %>%
+  dplyr::select(fac_type)
+
+## psych clinics
+sr_pc <- sf::st_read(dsn = file.path(home, ft_path), layer = "CHHS_psychclinics_csv_Events") %>%
+  st_transform(ca_crs) %>%
+  mutate(fac_type = "psychology clinic") %>%
+  dplyr::select(fac_type)
+
+## rehab clinics
+sr_rc <- sf::st_read(dsn = file.path(home, ft_path), layer = "CHHS_rehabclinic_csv_Events") %>%
+  st_transform(ca_crs) %>%
+  mutate(fac_type = "rehab") %>%
+  dplyr::select(fac_type)
+
+## skilled nursing facs
+sr_snf <- sf::st_read(dsn = file.path(home, ft_path), layer = "CHHS_skillednursingfacs_csv_Events") %>%
+  st_transform(ca_crs) %>%
+  mutate(fac_type = "skilled nursing facility") %>%
+  dplyr::select(fac_type)
+
+## surgical clinic
+sr_sc <- sf::st_read(dsn = file.path(home, ft_path), layer = "CHHS_surgicalclinic_csv_Events") %>%
+  st_transform(ca_crs)
+
+## acute care hospital
+sr_ach <- sf::st_read(dsn = file.path(home, ft_path), layer = "CHHS_acutecarehospital_csv_Events_1") %>%
+  st_transform(ca_crs) %>%
+  mutate(fac_type = "acute care hospital") %>%
+  dplyr::select(fac_type)
+
+## ca acute care hospital
+sr_caach <- sf::st_read(dsn = file.path(home, ft_path), layer = "CAAcuteCAreHostpitalslatlon_1") %>%
+  st_transform(ca_crs) %>%
+  mutate(fac_type = "general acute care hospital") %>%
+  dplyr::select(fac_type)
+
+## private schools
+sr_ps <- sf::st_read(dsn = file.path(home, ft_path), layer = "PrivSchoolsCA_1") %>%
+  st_transform(ca_crs) %>%
+  mutate(fac_type = "private school") %>%
+  dplyr::select(fac_type)
+
+## schools
+sr_s <- sf::st_read(dsn = file.path(home, ft_path), layer = "SchoolPropCA_1") %>%
+  st_transform(ca_crs) %>%
+  mutate(fac_type = "school") %>%
+  dplyr::select(fac_type)
+
+## SchoolsCA_Sabins_1
+sr_sca <- sf::st_read(dsn = file.path(home, ft_path), layer = "SchoolsCA_Sabins_1") %>%
+  st_transform(ca_crs) %>%
+  mutate(fac_type = "school") %>%
+  dplyr::select(fac_type)
+
+
+
+## functions
+## ----------------------------------------
+
+gen_within_vars <- function(dist_m) {
+  
+    # browser()
+    d <-  dist_m
+    
+    buff_d <- st_union(st_buffer(sr_dwellings, dist = d))
+    
+    buff_pts <- st_union(st_buffer(sr_dwellings, dist = d))
+    
+    
+    
+    wells_df <- wells_df %>% 
+      mutate(!!(paste0("within", d)) := (sf::st_within(wells_df, buf) %>% lengths() > 0 %>% as.numeric()))
+  }
+  wells_df
+}
+
+
+
+
+
+
+create_buffer <- function(dist_m) {
+  
+  buffer <- sr %>%
+    st_buffer(dist = dist_m) %>%
+    st_union() 
+  
+}
+
+
+gen_within_vars <- function(sr_df, wells_df, ft) {
+  for (i in 1:length(ft)){
+    # browser()
+    d = ft[i]
+    m = d*0.3048
+    buf <- st_buffer(st_geometry(sr_df), dist = m)
+    wells_df <- wells_df %>% 
+      mutate(!!(paste0("within", d)) := (sf::st_within(wells_df, buf) %>% lengths() > 0 %>% as.numeric()))
+  }
+  wells_df
+}
+
+
+
+## step 1: create buffer objects
+## ----------------------------------------
+
+buffer_dist_ft <- c(1000, 2500, 5280)
+ft_meter_val <- 0.3048
+
+buff_dist_m <- buffer_dist_ft * ft_meter_val
+
+
+
+
+
+
+
+
+
+
+
+
 
 # transform to NAD83(NSRS2007) / California Albers as well for wells and field boundaries
 wells <- sf::st_read(file.path(home, "emlab/projects/current-projects/calepa-cn/data/GIS/raw/allwells/Wells_All.shp")) %>% 
@@ -52,25 +233,9 @@ boundaries <- st_read(file.path(home, "emlab/projects/current-projects/calepa-cn
 #   st_sf(st_drop_geometry(X), geom = st_geometry(Y))
 # }
 
-create_buffer <- function(dist_ft) {
-  
-  buffer <- sr %>%
-    st_buffer(dist = dist_ft * ft_to_m) %>%
-    st_union() 
-  
-}
 
-gen_within_vars <- function(sr_df, wells_df, ft) {
-  for (i in 1:length(ft)){
-    # browser()
-    d = ft[i]
-    m = d*0.3048
-    buf <- st_buffer(st_geometry(sr_df), dist = m)
-    wells_df <- wells_df %>% 
-      mutate(!!(paste0("within", d)) := (sf::st_within(wells_df, buf) %>% lengths() > 0 %>% as.numeric()))
-  }
-  wells_df
-}
+
+
 
 gen_field_setback_coverage <- function(field_index, sr, ft = c(1000, 2500, 5280, 10000)) {
   field <- boundaries[field_index,]
