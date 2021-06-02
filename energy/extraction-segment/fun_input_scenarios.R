77077e2ea1f8c718e90ca67ddf4f0a0d0490d9f9
traceybit <traceymangin@gmail.com>
2021-06-02 16:56:33 -0700

    fix for merge

--- energy/extraction-segment/fun_input_scenarios.R
+++ energy/extraction-segment/fun_input_scenarios.R
@@ -73,7 +73,7 @@ load_scenarios_dt = function(oil_px_selection) {
   ghg_factors = ghg_factors[, .(doc_field_code, doc_fieldname, upstream_kgCO2e_bbl)]
   
   # load setback coverage file
-  setback_scens = fread(file.path(outputs_path, 'setback', 'model-inputs', setback_file), header = T) 
+  setback_scens = fread(file.path(outputs_path, 'setback', 'model-inputs', setback_file), header = T, colClasses = c('doc_field_code' = 'character')) 
   setback_scens <- setback_scens[, c("doc_field_code", "setback_scenario", "rel_coverage")]
   setnames(setback_scens, 'rel_coverage', 'area_coverage')
   # setback_scens[, V1 := NULL]
