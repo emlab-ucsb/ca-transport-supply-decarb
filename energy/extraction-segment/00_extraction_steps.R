5d2eb9d3f52431c935ca2c870811910fd4a3218d
traceybit <traceymangin@gmail.com>
2021-06-04 10:43:24 -0700

    remove plugged wells

--- energy/extraction-segment/00_extraction_steps.R
+++ energy/extraction-segment/00_extraction_steps.R
@@ -4,7 +4,7 @@
   # oil_price_selection    = args[1]
   oil_price_selection   = 'diagnostic'
     # choose from: reference, high, low, iea
-  run_type = "revised-setbacks"
+  run_type = "revised-remove-plugged"
     
 # outputs -------
   
