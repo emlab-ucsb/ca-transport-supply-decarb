d203de6b49692083c98a728a1f14688e07b67bd6
traceybit <traceymangin@gmail.com>
2021-06-24 17:06:08 -0700

    add old outputs, add maps

--- energy/extraction-segment/fun_benchmark.R
+++ energy/extraction-segment/fun_benchmark.R
@@ -2,16 +2,68 @@ benchmark_outputs <- function(oil_price_selection, output_extraction) {
   
   print('Now creating benchmarks...')
   
+  ## proj file
+  proj_dir <- '/Volumes/GoogleDrive/Shared drives/emlab/projects/current-projects/calepa-cn/'
+  
+  
   # create objects for list items -------
   
   state_out = output_extraction[[3]]
   
   field_out = output_extraction[[2]]
   
+  ## read in files
   extract_field_out <- fread('/Volumes/GoogleDrive/Shared drives/emlab/projects/current-projects/calepa-cn/outputs/predict-production/archive/scenarios_20_all_scens/download/field_level_prod_emissions_2020-2045.csv', header = T,
                              colClasses = ('doc_field_code' = 'character'))
 
+  report_wells_out <- fread("/Volumes/GoogleDrive/Shared drives/emlab/projects/current-projects/calepa-cn/outputs/predict-production/archive/scenarios_20_all_scens/well_entry_state_1977-2045.csv")
   
+  ## files
+  prod_file    <- "well_prod_m_processed.csv"
+  
+  # transform to NAD83(NSRS2007) / California Albers
+  # units will be in meters
+  ca_crs <- 3488
+  
+  ## ggplot plot, production
+  states <- st_as_sf(map("state", plot = FALSE, fill = TRUE))
+  
+  california <- states %>% filter(ID == "california") %>%
+    st_transform(ca_crs)
+  
+  ## counties boundaries
+  county_boundaries <- st_read(file.path(proj_dir, "data/GIS/raw/CA_Counties/CA_Counties_TIGER2016.shp")) %>% st_transform(ca_crs) %>%
+    dplyr::select(adj_county_name = NAME)
+  
+  ## monthly well production
+  well_prod <- fread(paste0(proj_dir, "data/stocks-flows/processed/", prod_file), colClasses = c('api_ten_digit' = 'character',
+                                                                                                 'doc_field_code' = 'character'))
+  ## county
+  county_lut <- well_prod %>%
+    dplyr::select(doc_field_code, county_name) %>%
+    unique() %>%
+    mutate(adj_county_name = str_remove(county_name, " Offshore"))
+  
+  
+  ## county prod all years
+  prod_x_county <- well_prod %>%
+    left_join(county_lut) %>%
+    group_by(doc_field_code, doc_fieldname, adj_county_name) %>%
+    summarise(prod = sum(OilorCondensateProduced, na.rm = T)) %>%
+    ungroup() %>%
+    group_by(doc_field_code) %>%
+    mutate(field_total = sum(prod, na.rm = T)) %>%
+    ungroup() %>%
+    mutate(rel_prod = prod / field_total) %>%
+    dplyr::select(doc_field_code, adj_county_name, rel_prod)
+  
+  ## county prod
+  county_prod <- well_prod %>%
+    filter(year == 2019) %>%
+    left_join(county_lut) %>%
+    group_by(year, adj_county_name) %>%
+    summarise(county_prod = sum(OilorCondensateProduced, na.rm = T)) %>%
+    ungroup() 
   
   
   ## all scenario combinations
@@ -126,7 +178,19 @@ benchmark_outputs <- function(oil_price_selection, output_extraction) {
 ## repeat for calepa outputs
 ## --------------------------------
   
+  
+  
   ## all scenarios, cal epa
+  report_wells_df <- rbind(oil_px_scens, innovation_scens, carbon_px_scens, ccs_cost_scens, setback_scens, quota_scens, tax_scens) %>%
+    select(scenario, oil_price_scenario:excise_tax_scenario) %>%
+    left_join(report_wells_out) %>%
+    filter(year >= 2020) %>%
+    mutate(oil_price_scenario = as.character(oil_price_scenario),
+           ccs_scenario = as.character(ccs_scenario),
+           type = 'total') %>%
+    rename(new_wells = new_wells_pred) %>%
+    select(-oil_price_usd_per_bbl)
+  
   report_df <- rbind(oil_px_scens, innovation_scens, carbon_px_scens, ccs_cost_scens, setback_scens, quota_scens, tax_scens) %>%
     select(scenario, oil_price_scenario:excise_tax_scenario) %>%
     left_join(extract_field_out) %>%
@@ -134,23 +198,23 @@ benchmark_outputs <- function(oil_price_selection, output_extraction) {
     mutate(oil_price_scenario = as.character(oil_price_scenario),
            ccs_scenario = as.character(ccs_scenario)) %>%
     group_by(scenario, oil_price_scenario, innovation_scenario, carbon_price_scenario, ccs_scenario, setback_scenario, prod_quota_scenario, excise_tax_scenario, year, well_type) %>%
-    summarise(new_wells = sum(as.numeric(n_wells), na.rm = T),
-              production_bbl = sum(as.numeric(production_bbl), na.rm = T),
+    summarise(production_bbl = sum(as.numeric(production_bbl), na.rm = T),
               ghg_kgCO2e = sum(as.numeric(upstream_kgCO2e), na.rm = T)) %>%
     ungroup() 
     
   report_df2 <- report_df %>%
     group_by(scenario, oil_price_scenario, innovation_scenario, carbon_price_scenario, ccs_scenario, setback_scenario, prod_quota_scenario, excise_tax_scenario, year) %>%
     summarise(production_bbl = sum(production_bbl, na.rm = T),
-              ghg_kgCO2e = sum(ghg_kgCO2e, na.rm = T),
-              new_wells = sum(new_wells, na.rm = T)) %>%
+              ghg_kgCO2e = sum(ghg_kgCO2e, na.rm = T)) %>%
     ungroup() %>%
     mutate(well_type = 'total') %>%
     rbind(report_df) %>%
     rename(type = well_type,
            production_bbls = production_bbl) %>%
-    mutate(ghg_MtCO2e = ghg_kgCO2e / 1e9) %>%
-    mutate(mean_emis_factor = ghg_kgCO2e / production_bbls) %>%
+    mutate(ghg_MtCO2e = ghg_kgCO2e / 1e9,
+           mean_emis_factor = ghg_kgCO2e / production_bbls,
+           year = as.integer(year)) %>%
+    left_join(report_wells_df) %>%
     mutate(option = ifelse(scenario == 'oil price scenarios', oil_price_scenario,
                            ifelse(scenario == 'innovation scenarios', innovation_scenario,
                                   ifelse(scenario == 'carbon price scenarios', carbon_price_scenario,
@@ -158,8 +222,7 @@ benchmark_outputs <- function(oil_price_selection, output_extraction) {
                                                 ifelse(scenario == 'setback scenarios', setback_scenario,
                                                        ifelse(scenario == 'quota scenarios', prod_quota_scenario, excise_tax_scenario))))))) %>%
     select(scenario, oil_price_scenario:excise_tax_scenario, year, type, production_bbls, new_wells, ghg_kgCO2e, ghg_MtCO2e, mean_emis_factor, option) %>%
-    mutate(version = "calepa",
-           year = as.integer(year)) 
+    mutate(version = "calepa") 
   
   
   ## all
@@ -331,21 +394,8 @@ benchmark_outputs <- function(oil_price_selection, output_extraction) {
     rel_heights = c(0.1, 1)
   )
   
-    }
-  
   }
   
-p1 <- plot_scen(1)
-p2 <- plot_scen(2)
-p3 <- plot_scen(3)
-p4 <- plot_scen(4)
-p5 <- plot_scen(5)
-p6 <- plot_scen(6)
-p7 <- plot_scen(7)
-
-macro_plot <- plot_grid(p1, p2, p3, p4, ncol = 1)
-policy_plot <- plot_grid(p5, p6, p7, ncol = 1)
-
   # save figures  -----
   
   # save info file
@@ -374,7 +424,113 @@ policy_plot <- plot_grid(p5, p6, p7, ncol = 1)
   embed_fonts(file.path(save_info_path, policy_fname),
               outfile = file.path(save_info_path, policy_fname))
   print(paste0('Saved diagnostic figures to ', policy_fname))
-
   
+  ## make spatial fig plots
+  ## change in production 2019 vs 2045 by county
+  pred_prod_county <- rbind(oil_px_scens, innovation_scens, carbon_px_scens, ccs_cost_scens, setback_scens, quota_scens, tax_scens) %>%
+    select(scenario, oil_price_scenario:excise_tax_scenario) %>%
+    left_join(field_out) %>%
+    filter(year == 2045) %>%
+    group_by(scenario, year, oil_price_scenario, innovation_scenario, carbon_price_scenario, ccs_scenario, setback_scenario, prod_quota_scenario, excise_tax_scenario, doc_field_code, doc_fieldname) %>%
+    summarise(prod = sum(total_prod_bbl, na.rm = T)) %>%
+    ungroup() %>%
+    left_join(prod_x_county) %>%
+    mutate(county_prod = prod * rel_prod) %>%
+    group_by(scenario, year, oil_price_scenario, innovation_scenario, carbon_price_scenario, ccs_scenario, setback_scenario, prod_quota_scenario, excise_tax_scenario, adj_county_name) %>%
+    summarise(county_prod = sum(county_prod, na.rm = T)) %>%
+    ungroup()
+  
+  ## all county
+  all_county_prod <- rbind(oil_px_scens, innovation_scens, carbon_px_scens, ccs_cost_scens, setback_scens, quota_scens, tax_scens) %>%
+    select(scenario, oil_price_scenario:excise_tax_scenario) %>%
+    mutate(year = 2019) %>%
+    full_join(county_prod) %>%
+    rbind(pred_prod_county) %>%
+    mutate(year = paste0('x', year)) %>%
+    pivot_wider(names_from = year, values_from = county_prod) %>%
+    mutate(diff_2045_2019 = x2045 - x2019,
+           rel_2045_2019 = diff_2045_2019 / x2019)
+  left_join(county_boundaries)
+  
+  ## plot
+  all_county_prod_df <- all_county_prod %>%
+    pivot_longer(diff_2045_2019:rel_2045_2019, names_to = 'metric', values_to = 'values') %>%
+    mutate(metric = ifelse(metric == 'diff_2045_2019', 'difference (bbls)', '% difference'),
+           adj_val = ifelse(metric == 'difference (bbls)', values / 1e6, values * 100)) %>%
+    mutate(innovation_scenario = as.character(innovation_scenario),
+           oil_price_scenario = as.character(oil_price_scenario)) %>%
+    mutate(option = ifelse(scenario == 'oil price scenarios', oil_price_scenario,
+                           ifelse(scenario == 'innovation scenarios', innovation_scenario,
+                                  ifelse(scenario == 'carbon price scenarios', carbon_price_scenario,
+                                         ifelse(scenario == 'CCS cost scenarios', ccs_scenario,
+                                                ifelse(scenario == 'setback scenarios', setback_scenario,
+                                                       ifelse(scenario == 'quota scenarios', prod_quota_scenario, excise_tax_scenario))))))) %>%
+    left_join(county_boundaries) 
+  
+  ## plot difference in production
+  comp_bbls <- ggplot() +
+    geom_sf(data = california, mapping = aes(fill = NULL), show.legend = FALSE) +
+    geom_sf(data = all_county_prod_df %>% filter(metric == 'difference (bbls)',
+                                                 scenario == name), mapping = aes(geometry = geometry, fill = adj_val), lwd = 0.25, show.legend = TRUE) +
+    scale_fill_gradient2(midpoint = 0, low = "red", mid = "white",
+                         high = "blue") +
+    labs(title = name,
+         fill = 'million bbls',
+         x = NULL,
+         y = NULL) +
+    facet_wrap(~option, ncol = 1) +
+    geom_sf_text(data = all_county_prod_df %>% filter(metric == 'difference (bbls)', scenario == name), aes(geometry = geometry, label = paste0(adj_county_name, '\n ', round(adj_val, digits = 2), ' mbbls')), colour = "black", size = 2) +
+    theme_bw() +
+    theme(legend.position = "bottom") 
+  
+  
+  ## perc
+  
+  comp_perc <- ggplot() +
+    geom_sf(data = california, mapping = aes(fill = NULL), show.legend = FALSE) +
+    geom_sf(data = all_county_prod_df %>% filter(metric != 'difference (bbls)', scenario == name), mapping = aes(geometry = geometry, fill = adj_val), lwd = 0.25, show.legend = TRUE) +
+    scale_fill_gradient2(midpoint = 0, low = "red", mid = "white",
+                         high = "blue") +
+    facet_wrap(~option, ncol = 1) +
+    labs(title = name,
+         fill = '% change',
+         x = NULL,
+         y = NULL) +
+    geom_sf_text(data = all_county_prod_df %>% filter(metric != 'difference (bbls)', scenario == name), aes(geometry = geometry, label = paste0(adj_county_name, '\n', round(adj_val), '%')), colour = "black", size = 2) +
+    theme_bw() +
+    theme(legend.position = "bottom") 
+  
+  
+  fig_comp <- plot_grid(comp_bbls, comp_perc, ncol = 2)
+  
+  ## save figures
+  comp_fname = paste0('change_prod_county_', name, '.pdf')
+  ggsave(fig_comp, 
+         filename = file.path(save_info_path, comp_fname), 
+         width = 20, 
+         height = 30)
+  
+  embed_fonts(file.path(save_info_path, comp_fname),
+              outfile = file.path(save_info_path, comp_fname))
+  print(paste0('Saved benchmark figures to ', comp_fname))
+  
+  
+  
+  }
+  
+  
+  
+p1 <- plot_scen(1)
+p2 <- plot_scen(2)
+p3 <- plot_scen(3)
+p4 <- plot_scen(4)
+p5 <- plot_scen(5)
+p6 <- plot_scen(6)
+p7 <- plot_scen(7)
+
+macro_plot <- plot_grid(p1, p2, p3, p4, ncol = 1)
+policy_plot <- plot_grid(p5, p6, p7, ncol = 1)
+
+
   
 } 
