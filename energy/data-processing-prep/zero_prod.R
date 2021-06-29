2ac69c7b1fc06ef62a2094c14fc665afe3e53493
traceybit <traceymangin@gmail.com>
2021-06-29 13:31:42 -0700

    update to group by api ten digit and field

--- energy/data-processing-prep/zero_prod.R
+++ energy/data-processing-prep/zero_prod.R
@@ -36,7 +36,7 @@ status <- all_wells %>%
 
 ## find wells that produce at some point over time period
 pos_well_api_prod <- well_prod %>%
-  group_by(api_ten_digit) %>%
+  group_by(api_ten_digit, doc_field_code) %>%
   summarise(oil_total = sum(OilorCondensateProduced, na.rm = T)) %>%
   ungroup() %>%
   mutate(pos_pro = ifelse(oil_total > 0, 1, 0)) %>%
@@ -44,21 +44,34 @@ pos_well_api_prod <- well_prod %>%
 
 pos_api_vec <- pos_well_api_prod$api_ten_digit
 
+## repeat, well-field level
+pos_api_field_prod <- well_prod %>%
+  mutate(api_field_code = paste0(api_ten_digit, "-", doc_field_code)) %>%
+  group_by(api_field_code) %>%
+  summarise(oil_total = sum(OilorCondensateProduced, na.rm = T)) %>%
+  ungroup() %>%
+  mutate(pos_pro = ifelse(oil_total > 0, 1, 0)) %>%
+  filter(pos_pro == 1)
+
+pos_api_field_vec <- pos_api_field_prod$api_field_code
+
 ## well add column for month_year, select certain columns
-well_prod2 <- well_prod[, .(api_ten_digit, month_year, OilorCondensateProduced, GasProduced, WaterProduced)]
+well_prod2 <- well_prod[, .(api_ten_digit, doc_field_code, month_year, OilorCondensateProduced, GasProduced, WaterProduced)]
+
+well_prod2[, api_field_code := paste0(api_ten_digit, "-", doc_field_code)]
 
 setnames(well_prod2, c("OilorCondensateProduced", "GasProduced", "WaterProduced"),
          c("oil_prod", "gas_prod", "water_prod"))
 
 
 ## start making stuff for histogram
-prod_dt <- well_prod2[api_ten_digit %chin% pos_api_vec]
+prod_dt <- well_prod2[api_field_code %chin% pos_api_field_vec]
 
-prod_dt <- prod_dt[, lapply(.SD, sum, na.rm = T), .SDcols = c("oil_prod", "gas_prod", "water_prod"), by = .(api_ten_digit, month_year)]
+prod_dt <- prod_dt[, lapply(.SD, sum, na.rm = T), .SDcols = c("oil_prod", "gas_prod", "water_prod"), by = .(api_field_code, month_year)]
 
 
 ##
-well_year_combos <- expand.grid(api_ten_digit = unique(prod_dt$api_ten_digit),
+well_year_combos <- expand.grid(api_field_code = unique(prod_dt$api_field_code),
                                 month_year = unique(prod_dt$month_year)) 
 
 setDT(well_year_combos)
@@ -67,7 +80,7 @@ prod_dt <- merge(well_year_combos, prod_dt, all = TRUE)
 
 prod_dt[is.na(prod_dt)] <- 0
 
-setorder(prod_dt, api_ten_digit, month_year)
+setorder(prod_dt, api_field_code, month_year)
 
 # prod_dt[, oil_or_gas_prod := fifelse(oil_prod > 0 | gas_prod > 0, 1, 0)]
 # 
@@ -77,9 +90,9 @@ setorder(prod_dt, api_ten_digit, month_year)
 prod_dt[, `:=` (zero_prod = fifelse(oil_prod > 0, 0, 1),
                 pos_prod = fifelse(oil_prod > 0, 1, 0))]
 
-prod_dt[, cumul_zero_prod := hutilscpp::cumsum_reset(as.logical(zero_prod), zero_prod), api_ten_digit]
+prod_dt[, cumul_zero_prod := hutilscpp::cumsum_reset(as.logical(zero_prod), zero_prod), api_field_code]
 
-prod_dt[, max_of_grp := shift(cumul_zero_prod, type = "lead"), by = api_ten_digit]
+prod_dt[, max_of_grp := shift(cumul_zero_prod, type = "lead"), by = api_field_code]
 
 prod_dt[, max_of_grp := fifelse(is.na(max_of_grp), 0, max_of_grp)]
 
@@ -87,9 +100,9 @@ prod_dt[, max_of_grp2 := fifelse(cumul_zero_prod > 0 & max_of_grp == 0, cumul_ze
 
 prod_dt[, zero_prod_end := fifelse(month_year == max(month_year) & max_of_grp2 > 0, 1, 0)]
 
-prod_dt[, cumul_prod := cumsum(pos_prod), api_ten_digit]
+prod_dt[, cumul_prod := cumsum(pos_prod), api_field_code]
 
-prod_dt[, cumul_prod_next := shift(cumul_prod, type = "lead"), api_ten_digit]
+prod_dt[, cumul_prod_next := shift(cumul_prod, type = "lead"), api_field_code]
 
 ## use this to remove zero production before first production date in historic data set
 prod_dt[, zero_prod_start := fifelse(max_of_grp2 > 0 & cumul_prod_next == 1, 1, 0)]
@@ -105,6 +118,8 @@ zero_prod_dt_filt <- zero_prod_dt[zero_prod_start != 1]
 
 setDT(status)
 
+zero_prod_dt_filt[, api_ten_digit := substr(api_field_code, 1, 10)]
+
 zero_prod_dt_filt2 <- setDT(left_join(zero_prod_dt_filt, status, by = "api_ten_digit"))
 
 ## remove tail of dataset.... 
@@ -115,6 +130,9 @@ zero_prod_dt_filt2[, remove_tail_all := fifelse(zero_prod_end == 1, 1, 0)]
 # zero_prod_dt_filt3 <- zero_prod_dt_filt2[remove_tail == 0]
 
 setnames(zero_prod_dt_filt2, "max_of_grp2", "zero_prod_months")
+
+## add doc field code
+zero_prod_dt_filt2[, doc_field_code := stringr::str_sub(api_field_code, - 3, - 1)]
 
 ## make histogram
 
@@ -131,17 +149,17 @@ calc_zero_prod <- function(n_month_val) {
   tmp_all <- zero_prod_dt_filt2[zero_prod_months >= n_month_val]
 
   ## prod stop
-  tmp_zero_prod <- tmp_all[, n := .N, by = .(api_ten_digit)]
+  tmp_zero_prod <- tmp_all[, n := .N, by = .(api_field_code)]
   
   ## those that stop after n months
   tmp_stop_prod <- tmp_zero_prod[zero_prod_end == 1 & n == 1]
   
-  tmp_n_stop_n <- length(unique(tmp_stop_prod[, api_ten_digit]))
+  tmp_n_stop_n <- length(unique(tmp_stop_prod[, api_field_code]))
   
   ## n pause and stop
   tmp_break_stop <- tmp_zero_prod[zero_prod_end == 1 & n > 1]
   
-  tmp_n_break_stop_n <- length(unique(tmp_break_stop[, api_ten_digit]))
+  tmp_n_break_stop_n <- length(unique(tmp_break_stop[, api_field_code]))
   
   ## n plugged in stop 
   tmp_stop_status <- tmp_stop_prod[well_status == "Plugged", c(n = .N), by = .(well_status)]
@@ -153,13 +171,13 @@ calc_zero_prod <- function(n_month_val) {
   
   ## break and then produce 
   tmp_break <- tmp_zero_prod[zero_prod_end != 1]
-  tmp_break <- tmp_break[!(api_ten_digit %in% c(tmp_stop_prod[, api_ten_digit], tmp_break_stop[, api_ten_digit]))]
+  tmp_break <- tmp_break[!(api_field_code %in% c(tmp_stop_prod[, api_field_code], tmp_break_stop[, api_field_code]))]
   
-  tmp_n_break_vec <- length(unique(tmp_break[, api_ten_digit]))
+  tmp_n_break_vec <- length(unique(tmp_break[, api_field_code]))
   
   ## total wells in threshold
   tmp_all <- zero_prod_dt_filt2[zero_prod_months >= n_month_val]
-  tmp_n_total <- length(unique(tmp_all[, api_ten_digit]))
+  tmp_n_total <- length(unique(tmp_all[, api_field_code]))
 
   ## make data table
   out_df <- data.table(zero_prod_length = n_month_val,
@@ -193,7 +211,7 @@ filt_zero_prod <- function(n_month_val) {
   
   ## make data table
   out_df <- tmp_zero_prod %>% 
-    select(api_ten_digit, zero_prod_months, well_status) %>%
+    select(api_field_code, api_ten_digit, doc_field_code, zero_prod_months, well_status) %>%
     mutate(year_cut_off = n_month_val / 12)
   
 }
