99afff042894c25c8037d0f0f29c216fd15a4e5e
traceybit <traceymangin@gmail.com>
2021-07-06 12:18:29 -0700

    correct label

--- energy/extraction-segment/fun_diagnostic_plot.R
+++ energy/extraction-segment/fun_diagnostic_plot.R
@@ -185,10 +185,10 @@ plot_diagnostic_outputs <- function(oil_price_selection, output_extraction) {
   
   # read in baseline for comparison ------
   
-  state_base = fread(file.path(base_path, 'extraction_2021-07-01', 'revised-add-density', 'diagnostic-state-level-results.csv'), header = T)
+  state_base = fread(file.path(base_path, 'extraction_2021-07-01', 'revised-annual-ghg-factors', 'diagnostic-state-level-results.csv'), header = T)
   
   # add column for base case
-  state_base[, version := "density tracking"]
+  state_base[, version := "revised ghg factors"]
   
   # bind data
   state_all <- rbind(state_base, state_out)
