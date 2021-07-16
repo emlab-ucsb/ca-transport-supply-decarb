084057c35bf03e2f897f3a1c91737208fbd81bd5
traceybit <traceymangin@gmail.com>
2021-07-16 14:14:18 -0700

    review exit outputs and compare production (hist vs projected)

--- energy/misc/prod_comparison.R
+++ energy/misc/prod_comparison.R
@@ -5,10 +5,11 @@
 ## libraries
 library(data.table)
 library(tidyverse)
+library(scales)
 
 ## paths
 outputs_path      = '/Volumes/GoogleDrive/Shared drives/emlab/projects/current-projects/calepa-cn/outputs'
-projection_path   = 'predict-production/extraction_2021-07-07/revised-density-calc'
+projection_path   = 'predict-production/extraction_2021-07-15/revised-edit-exit-model'
 
 ## files
 histprod_file   = 'stocks-flows/crude_prod_x_field_revised.csv'
@@ -18,5 +19,61 @@ model_out       = 'diagnostic-field-level-results.csv'
 prod_hist = fread(file.path(outputs_path, histprod_file), header = T, colClasses = c("doc_field_code" = "character"))
 projected_prod = fread(file.path(outputs_path, projection_path, model_out), header = T, colClasses = c("doc_field_code" = "character"))
 
-## 2019 and 2020 production
+## bau production
+
+bau_prod = projected_prod[prod_quota_scenario == "no quota" & setback_scenario == "no_setback"]
+bau_prod = bau_prod[, .(doc_field_code, year, total_prod_bbl)]
+setnames(bau_prod, "total_prod_bbl", "total_bbls")
+
+## 2019 vs 2020
+comp_19_20 = prod_hist[year == 2019]
+comp_19_20[, year := NULL]
+setnames(comp_19_20, "total_bbls", "bbls_2019")
+
+comp_20 = bau_prod[year == 2020]
+comp_20[, year := NULL]
+setnames(comp_20, "total_bbls", "bbls_2020")
+
+comp_19_20 = merge(comp_19_20, comp_20,
+                   all.x = T)
+
+comp_19_20[, bbls_2020 := fifelse(is.na(bbls_2020), 0, bbls_2020)]
+
+comp_19_20 <- comp_19_20 %>%
+  pivot_longer(bbls_2019:bbls_2020, names_to = "year", values_to = "bbls") %>%
+  mutate(year = ifelse(year == "bbls_2019", 2019, 2020),
+         year = as.integer(year))
+
+
+## make plot
+year_comp <- ggplot(comp_19_20, aes(x = year, y = bbls, group = doc_fieldname)) +
+  geom_line() +
+  labs(x = NULL,
+      y = "barrels") +
+  geom_point(data = comp_19_20, aes(x = year, y = bbls, group = doc_fieldname)) +
+  geom_text(data = comp_19_20 %>% filter(year == 2020), aes(x = year, label = doc_fieldname)) +
+  scale_y_continuous(labels = comma) + 
+  scale_x_continuous(breaks=c(2019, 2020)) +
+  theme_bw()
+
+## cumulative
+hist_20 = prod_hist[year >= 2015, .(cumul_hist = sum(total_bbls)), by = .(doc_field_code, doc_fieldname)]
+pred_20 = bau_prod[year <=  2024, .(cumul_projected = sum(total_bbls)), by = .(doc_field_code)]
+
+cumul_comp <- full_join(hist_20, pred_20)
+cumul_comp[is.na(cumul_comp)] = 0
+
+cumul_comp <- cumul_comp %>%
+  pivot_longer(cumul_hist:cumul_projected, names_to = "period", values_to = "bbls") %>%
+  mutate(period = ifelse(period == "cumul_hist", "historic", "projected"))
+
+## make plot
+year_comp <- ggplot(cumul_comp, aes(x = period, y = bbls / 1e6, group = doc_fieldname)) +
+  geom_line() +
+  labs(x = NULL,
+       y = "million bbls") +
+  geom_point(data = cumul_comp, aes(x = period, y = bbls  / 1e6, group = doc_fieldname)) +
+  geom_text(data = cumul_comp %>% filter(period == "projected"), aes(x = "projected", label = doc_fieldname), hjust = 0) +
+  theme_bw()
+
 
--- energy/misc/review_exit_outputs.R
+++ energy/misc/review_exit_outputs.R
@@ -4,38 +4,141 @@
 
 library(tidyverse)
 library(data.table)
+library(hrbrthemes)
+library(scales)
+library(extrafont)
+library(ggrepel)
 
 ## set paths and file names
-model_out_path      = '/Volumes/GoogleDrive/Shared drives/emlab/projects/current-projects/calepa-cn/outputs/predict-production'
-run_folder          = 'extraction_2021-07-12/revised-incorporate-exit-v2'
+model_out_path      = '/Volumes/GoogleDrive/Shared drives/emlab/projects/current-projects/calepa-cn/outputs/predict-production/'
+run_folder          = 'extraction_2021-07-15/revised-edit-exit-model/'
 exit_fname          = 'diagnostic-exit-results.csv'
-save_info_path      = paste0(model_out_path, "/", run_folder, '/diagnostic-figs/exit-figs-x-field')
+field_fname         = "diagnostic-field-level-results.csv"
+save_exit_path      = '/Volumes/GoogleDrive/Shared drives/emlab/projects/current-projects/calepa-cn/outputs/exit/'
+exit_file           = 'well_exits_under_rule_forecast.csv'
+save_info_path      = paste0(model_out_path, "/", run_folder, 'diagnostic-figs/exit-figs-x-field')
+
+
+## new well entry by field
+field_out <- fread(paste0(model_out_path, run_folder, field_fname), header = T, colClasses = c('doc_field_code' = 'character'))
+field_out[, scen_name := fifelse(setback_scenario == "setback_2500ft", "LCE2",
+                                 fifelse(setback_scenario == "no_setback" & prod_quota_scenario == "quota_20", "LCE1", "BAU"))]
+
+new_wells <- field_out[, .(scen_name, doc_field_code, doc_fieldname, year, new_wells)]
+
+## previous exit
+prev_exits <- fread(paste0(save_exit_path, exit_file), header = T, colClasses = c('doc_field_code' = 'character'))
+
+## all fields and exit years
+prev_exits2 <- expand.grid(doc_field_code = unique(prev_exits$doc_field_code),
+                          setback_scenario = unique(prev_exits$setback_scenario),
+                          exit_year = seq(from = 2020, to = 2045, by = 1)) %>%
+  left_join(prev_exits) %>%
+  select(-doc_fieldname) %>%
+  mutate(no_exits_field = ifelse(is.na(no_exits_field), 0, no_exits_field))
+
+## state level
+prev_exits_state <- prev_exits2 %>%
+  group_by(setback_scenario, year = exit_year) %>%
+  summarise(sum_exits = sum(no_exits_field)) %>%
+  ungroup() %>%
+  mutate(scen_name = NA,
+         version = "exit-rule") %>%
+  select(scen_name, setback_scenario, year, n_exits = sum_exits, version)
+
 
 ## read in outputs
 exit_out <- fread(file.path(model_out_path, run_folder, exit_fname), header = T, colClasses = c('doc_field_code' = 'character'))
 
+exit_out <- exit_out[setback_scenario %in% c("no_setback", "setback_1000ft", "setback_2500ft")]
+
 exit_out[, scen_name := fifelse(setback_scenario == "setback_2500ft", "LCE2",
                                fifelse(setback_scenario == "no_setback" & prod_quota_scenario == "quota_20", "LCE1", "BAU"))]
 
-exit_out2 <- exit_out %>%
-  mutate(well_type = ifelse(vintage == "new", "new", "existing")) %>%
-  group_by(scen_name, doc_field_code, doc_fieldname, well_type, year, n_well_exit) %>%
-  summarise(num_wells_start = sum(adj_no_wells),
-            num_wells_after_exit = sum(no_wells_after_exit)) %>%
-  ungroup() %>%
-  arrange(scen_name, doc_field_code, well_type, year) 
+# ## compare 2020 predicted well exit
+# pred_2020 <- exit_out[year == 2020, .(scen_name, doc_field_code, orig_year, orig_year, vintage, start_year, n_well_exit)]
+# 
+# pred_2020_2 <- pred_2020 %>%
+#   pivot_wider(names_from = scen_name, values_from = n_well_exit) %>%
+#   mutate(same = ifelse(BAU == LCE1 & BAU == LCE2, 1, 0))
+# ## prediction is the same
+# 
+# ## no wells after exit... same?
+# nafter <- pred_2020 <- exit_out[year == 2020, .(scen_name, doc_field_code, orig_year, vintage, start_year, no_wells_after_exit)]
+# 
+# nafter2 <- nafter %>%
+#   pivot_wider(names_from = scen_name, values_from = no_wells_after_exit) %>%
+#   mutate(bau_lce1 = ifelse(BAU == LCE1, 1, 0))
+
+# ###
+# exit_out2 <- exit_out %>%
+#   mutate(well_type = ifelse(vintage == "new", "new", "existing")) %>%
+#   group_by(scen_name, doc_field_code, doc_fieldname, well_type, year, n_well_exit) %>%
+#   summarise(num_wells_start = sum(adj_no_wells),
+#             num_wells_after_exit = sum(no_wells_after_exit)) %>%
+#   ungroup() %>%
+#   arrange(scen_name, doc_field_code, well_type, year) 
 
 exit_all <- exit_out %>%
-  mutate(well_type = "all_wells") %>%
-  group_by(scen_name, doc_field_code, doc_fieldname, well_type, year, n_well_exit) %>%
-  summarise(num_wells_start = sum(adj_no_wells),
-            num_wells_after_exit = sum(no_wells_after_exit)) %>%
+  select(scen_name, doc_field_code, doc_fieldname, setback_scenario, year, orig_year, vintage, start_year, adj_no_wells, no_wells_after_exit) %>%
+  group_by(scen_name, doc_field_code, doc_fieldname, setback_scenario, vintage, start_year) %>%
+  mutate(prev_n_wells = ifelse(start_year >= 2020 & start_year == orig_year, adj_no_wells, 
+                               ifelse(start_year < 2020 & orig_year == 2020, adj_no_wells, lag(no_wells_after_exit))),
+         n_exits = prev_n_wells - no_wells_after_exit) %>%
   ungroup() %>%
-  arrange(scen_name, doc_field_code, well_type, year) %>%
-  rbind(exit_out2) %>%
-  select(-num_wells_start) %>%
-  mutate(well_type = ifelse(well_type == "all_wells", "All wells",
-                            ifelse(well_type == "existing", "Existing wells", "New wells")))
+  group_by(scen_name, doc_field_code, doc_fieldname, setback_scenario, orig_year) %>%
+  summarise(prev_n_wells = sum(prev_n_wells),
+            n_exits = sum(n_exits)) %>%
+  ungroup() %>%
+  arrange(scen_name, doc_field_code, orig_year) 
+
+proj_exit_state <- exit_all %>%
+  group_by(scen_name, setback_scenario, orig_year) %>%
+  summarise(n_exits = sum(n_exits)) %>%
+  ungroup() %>%
+  mutate(version = "exit-model") %>%
+  rename(year = orig_year) %>%
+  rbind(prev_exits_state) %>%
+  mutate(setback_scenario = ifelse(setback_scenario %in% c("no_setback", "setback_2500ft"), setback_scenario, paste0(setback_scenario, "ft")),
+         scen_name = ifelse(is.na(scen_name), "no-scen", scen_name),
+         id = paste0(scen_name, "-", setback_scenario))
+
+## look at number of exits in year 2020 across scenarios
+scen_comp <- exit_out[, .(scen_name, doc_field_code, doc_fieldname, year, orig_year, vintage, start_year, adj_no_wells, no_wells_after_exit)]
+
+scen_comp[, prev_n_wells := fifelse(orig_year == 2020, adj_no_wells, lag(no_wells_after_exit))]
+scen_comp[, n_exits := prev_n_wells - no_wells_after_exit]
+scen_comp[, c("prev_n_wells", "no_wells_after_exit", "adj_no_wells") := NULL]
+
+# scen_comp <- scen_comp %>%
+#   select(-orig_year) %>%
+#   group_by(scen_name, doc_field_code, doc_fieldname, year, vintage, start_year) %>%
+#   pivot_wider(names_from = scen_name, values_from = n_exits) %>%
+#   mutate(BAU = ifelse(is.na(BAU), 0, BAU),
+#          LCE1 = ifelse(is.na(LCE1), 0, LCE1),
+#          LCE2 = ifelse(is.na(LCE2), 0, LCE2)) %>%
+#   mutate(diff1 = BAU - LCE1,
+#          diff2 = BAU - LCE2,
+#          diff3 = LCE1 - LCE2)
+
+## new wells exit
+exit_new <- exit_out %>%
+  select(scen_name, doc_field_code, doc_fieldname, setback_scenario, year, orig_year, vintage, start_year, adj_no_wells, no_wells_after_exit) %>%
+  group_by(scen_name, doc_field_code, doc_fieldname, setback_scenario, vintage, start_year) %>%
+  mutate(prev_n_wells = ifelse(start_year >= 2020 & start_year == orig_year, adj_no_wells, 
+                               ifelse(start_year < 2020 & orig_year == 2020, adj_no_wells, lag(no_wells_after_exit))),
+         n_exits = prev_n_wells - no_wells_after_exit) %>%
+  ungroup() %>%
+  filter(start_year >= 2020) %>%
+  select(scen_name, doc_field_code, doc_fieldname, orig_year, vintage, start_year, n_exits) %>%
+  mutate(vintage_name = paste0(vintage, "-", start_year))
+
+
+
+
+## new wells and well exits
+field_entry_exit <- merge(exit_all2[, c('scen_name', 'doc_field_code', 'doc_fieldname', 'year', 'n_exits')], new_wells,
+                          by = c("scen_name", "doc_field_code", "doc_fieldname", "year"))
 
 
 ## theme
@@ -100,5 +203,90 @@ plot_wells <- function(field) {
 field_names <- unique(exit_all$doc_fieldname)
 
 purrr::map(field_names, plot_wells)
-  
+
+
+## plot state level
+proj_exit_state_plot_df <- proj_exit_state %>% filter(!setback_scenario %in% c("setback_1000ft", "setback_5280ft")) %>% 
+  mutate(scen_name = ifelse(scen_name == 'no-scen', 'exit-rule', scen_name))
+
+proj_exit_state_plot_df$scen_name <- factor(proj_exit_state_plot_df$scen_name, 
+                                               levels = c("exit-rule", "BAU", "LCE1", "LCE2"))
+
+
+state_comp <- ggplot(proj_exit_state_plot_df, 
+                     aes(x = year, y = n_exits, group = id, color = scen_name)) +
+  geom_line(alpha = 0.75) +
+  labs(x = NULL,
+       y = "number of wells that exit") +
+  facet_wrap(~setback_scenario) +
+  scale_y_continuous(labels = comma) +
+  # geom_text_repel(data = proj_exit_state_plot_df %>% filter(year == 2021), aes(year, n_exits, label = scen_name)) +
+  theme_bw() +
+  theme_line +
+  theme(legend.title = element_blank())
+
+ggsave(state_comp,
+       filename = file.path(model_out_path, run_folder, "diagnostic-figs/state_exit_comparison.pdf"),
+       width = 6,
+       height = 6)
+
+embed_fonts(file.path(model_out_path, run_folder, "diagnostic-figs/state_exit_comparison.pdf"),
+            outfile = file.path(model_out_path, run_folder, "diagnostic-figs/state_exit_comparison.pdf"))  
+
+
+## make figure that looks at well entry and well exit
+## ---------------------------------------------------
+
+## exit by well type
+exit_new_existing <- exit_out %>%
+  select(scen_name, doc_field_code, doc_fieldname, setback_scenario, year, orig_year, vintage, start_year, adj_no_wells, no_wells_after_exit) %>%
+  group_by(scen_name, doc_field_code, doc_fieldname, setback_scenario, vintage, start_year) %>%
+  mutate(prev_n_wells = ifelse(start_year >= 2020 & start_year == orig_year, adj_no_wells, 
+                               ifelse(start_year < 2020 & orig_year == 2020, adj_no_wells, lag(no_wells_after_exit))),
+         n_exits = prev_n_wells - no_wells_after_exit) %>%
+  ungroup() %>%
+  mutate(vintage = ifelse(start_year < 2020, "existing", "new")) %>%
+  group_by(scen_name, doc_field_code, doc_fieldname, setback_scenario, orig_year, vintage) %>%
+  summarise(prev_n_wells = sum(prev_n_wells),
+            n_exits = sum(n_exits)) %>%
+  ungroup() %>%
+  arrange(scen_name, doc_field_code, orig_year) %>%
+  rename(year = orig_year) %>%
+  pivot_wider(names_from = vintage, values_from = c(prev_n_wells, n_exits)) %>%
+  select(scen_name:prev_n_wells_existing, n_exits_existing, prev_n_wells_new, n_exits_new) %>%
+  left_join(new_wells) %>%
+  ## shift new wells down
+  group_by(scen_name, doc_field_code, doc_fieldname) %>%
+  mutate(new_wells_lag = lag(new_wells)) %>%
+  ungroup() %>%
+  select(-setback_scenario) %>%
+  select(scen_name:n_exits_existing, prev_n_wells_new, new_wells, new_wells_lag, n_exits_new)
+
+## try to isolate instances of entry and immediate exit
+exit_new_existing2 <- exit_new_existing %>%
+  mutate(diff = new_wells_lag - n_exits_new) %>%
+  filter(n_exits_new > 0 & new_wells_lag > 0 & diff == 0)
+
+
+# new wells
+new_wells_fig = ggplot(state_all_long %>% filter(type == "new"), 
+                       aes(x = year, y = new_wells, color = version)) + 
+  geom_line(alpha = 0.7, size = 1) +
+  facet_grid(~ scen_name) +
+  geom_point(data = state_all_long %>% filter(version == paste0("adj-", run_type), year %in% seq(2020, 2045, by = 5)),
+             aes(x = year, y = new_wells, color = version), shape = 3) +
+  geom_line(data = report_wells_out_all %>% mutate(version = ifelse(year <= 2019, "historic", "calepa-report")), aes(x = year, y = new_wells, color = version), alpha = 0.7, size = 1) +
+  geom_vline(xintercept = 2019, color = "darkgrey", size = 0.3, lty = "dashed") +
+  scale_color_manual(values = c("#FF6E1B", "#FFD200", "#005581", "black")) +
+  labs(title = 'State-level new wells',
+       subtitle = 'number of new wells', 
+       x = 'Year',
+       y = NULL,
+       color = '') +
+  # scale_color_gradient(low = "#f7fbff", high = "#08306b") +
+  theme_line
+
+
+
+
   
