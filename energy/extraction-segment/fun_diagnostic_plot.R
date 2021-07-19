46a792e46fa9fdd9897b92f51223b2c933dd210f
traceybit <traceymangin@gmail.com>
2021-07-19 15:00:32 -0700

    prepare for new run, exit model after entry

--- energy/extraction-segment/fun_diagnostic_plot.R
+++ energy/extraction-segment/fun_diagnostic_plot.R
@@ -185,10 +185,10 @@ plot_diagnostic_outputs <- function(oil_price_selection, output_extraction) {
   
   # read in baseline for comparison ------
   
-  state_base = fread(file.path(base_path, 'extraction_2021-07-07', 'revised-density-calc', 'diagnostic-state-level-results.csv'), header = T)
+  state_base = fread(file.path(base_path, 'extraction_2021-07-15', 'revised-edit-exit-model', 'diagnostic-state-level-results.csv'), header = T)
   
   # add column for base case
-  state_base[, version := "pre-exit-model-or-decline-update"]
+  state_base[, version := "exit-first"]
   
   # bind data
   state_all <- rbind(state_base, state_out)
