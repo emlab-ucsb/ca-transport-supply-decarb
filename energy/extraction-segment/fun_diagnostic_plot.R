4ffa723149ed2fe79f58c5316d08c9d5b6b0fac8
traceybit <traceymangin@gmail.com>
2021-07-21 11:34:52 -0700

    run model adjusting number of new wells for setbacks instead of new peak production

--- energy/extraction-segment/fun_diagnostic_plot.R
+++ energy/extraction-segment/fun_diagnostic_plot.R
@@ -185,10 +185,10 @@ plot_diagnostic_outputs <- function(oil_price_selection, output_extraction) {
   
   # read in baseline for comparison ------
   
-  state_base = fread(file.path(base_path, 'extraction_2021-07-19', 'revised-exit-after-entryl', 'diagnostic-state-level-results.csv'), header = T)
+  state_base = fread(file.path(base_path, 'extraction_2021-07-21', 'revised-update-oil-price', 'diagnostic-state-level-results.csv'), header = T)
   
   # add column for base case
-  state_base[, version := "exit-after-entry"]
+  state_base[, version := "update-oil-price"]
   
   # bind data
   state_all <- rbind(state_base, state_out)
