011f32e8f4da5902bd9ef22c838e30f9660ff904
traceybit <traceymangin@gmail.com>
2021-07-22 15:21:55 -0700

    create df of fields for health and labor team

--- energy/compile-outputs/extraction_fields.R
+++ energy/compile-outputs/extraction_fields.R
@@ -5,15 +5,20 @@
 ## libraries
 library(data.table)
 library(tidyverse)
+library(sf)
 
 ## set paths and file names
+home                = "/Volumes/GoogleDrive/Shared drives"
 model_out_path      = '/Volumes/GoogleDrive/Shared drives/emlab/projects/current-projects/calepa-cn/outputs/predict-production/'
-run_folder          = 'extraction_2021-07-21/revised-new-wells-setback/'
+run_folder          = 'extraction_2021-07-22/revised-ccs-correct-new-setback/'
 field_fname         = "diagnostic-field-level-results.csv"
 
-# save_exit_path      = '/Volumes/GoogleDrive/Shared drives/emlab/projects/current-projects/calepa-cn/outputs/exit/'
-# exit_file           = 'well_exits_under_rule_forecast.csv'
-# save_info_path      = paste0(model_out_path, "/", run_folder, 'diagnostic-figs/exit-figs-x-field')
+## save path
+save_path           = '/Volumes/GoogleDrive/Shared drives/emlab/projects/current-projects/calepa-cn/outputs/academic-out/extraction/'
+
+## field boundaries
+boundaries <- st_read(file.path(home, "emlab/projects/current-projects/calepa-cn/data/GIS/raw/field-boundaries/DOGGR_Admin_Boundaries_Master.shp")) %>% st_transform(3488)
+
 
 ## read in output file
 field_out <- fread(paste0(model_out_path, run_folder, field_fname), header = T, colClasses = c('doc_field_code' = 'character'))
@@ -33,5 +38,10 @@ pos_field <- field_out %>%
 field_out_zero <- field_out %>%
   filter(!doc_field_code %in% pos_field$doc_field_code)
 
+field_boundaries <- boundaries %>%
+  filter(FIELD_CODE %in% pos_field$doc_field_code) %>%
+  dplyr::select(NAME, doc_field_code = FIELD_CODE)
 
+## save for health/labor team
+st_write(field_boundaries, dsn = paste0(save_path, "extraction_fields.shp"))
 
