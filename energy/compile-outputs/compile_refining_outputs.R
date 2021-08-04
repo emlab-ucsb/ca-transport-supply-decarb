6ba1ff2a7c0d0b6e1b6d2219c12a8891337b362f
traceybit <traceymangin@gmail.com>
2021-08-04 15:26:54 -0700

    review exit model

--- energy/compile-outputs/compile_refining_outputs.R
+++ energy/compile-outputs/compile_refining_outputs.R
@@ -9,15 +9,15 @@ library(openxlsx)
 ## paths
 main_path <- "/Volumes/GoogleDrive/Shared\ drives/emlab/projects/current-projects/calepa-cn"
 data_path  <-'data/stocks-flows/processed'
-
+outputs_path <- 'model-development/scenario-plot/refinery-outputs'
 
 ## files
 oil_price_file <- 'oil_price_projections_revised.xlsx'
 refining_file <- 'refining_scenario_outputs_refinery_net_exports_revised.csv'
 
 
-
 ## read in files
+## ---------------------------------------
 
 ## oil prices
 oilpx_scens = setDT(read.xlsx(file.path(main_path, data_path, oil_price_file), sheet = 'real', cols = c(1, 7:9)))
@@ -28,3 +28,14 @@ oilpx_scens[, oil_price_scenario := gsub('_', ' ', oil_price_scenario)]
 oilpx_scens[, oil_price_scenario := factor(oil_price_scenario, levels = c('reference case', 'high oil price', 'low oil price'))]
 oilpx_scens <- oilpx_scens[year >= 2019]
 setorderv(oilpx_scens, c('oil_price_scenario', 'year'))
+
+
+## refining outputs
+refining_out <- fread(file.path(main_path, outputs_path, refining_file))
+
+## site ids 
+site_id <- read_csv(paste0(calepa_path, "data/stocks-flows/processed/refinery_loc_cap.csv")) %>%
+  select(site_id, county)
+
+
+
