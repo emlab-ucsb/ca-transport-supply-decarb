d5aacd3e0ad64880694d08b622353b35d3dbbbe0
Vincent Thivierge <vthivierge@ucsb.edu>
2021-08-25 18:49:13 -0400

    Last minute changes to emission factors

--- health/scripts/emission-factors.R
+++ health/scripts/emission-factors.R
@@ -1,7 +1,7 @@
-# CalEPA: Emission factors
+# CalEPA: Emission factors from Jaramillo and Muller (2009)
 # vthivierge@ucsb.edu
 # created: 08/11/2020
-# updated: 08/11/2020
+# updated: 08/25/2020
 
 
 # set up environment
@@ -23,14 +23,14 @@ for (i in packages) {
 }
 ## Directory
 
-setwd(dirname(rstudioapi::getActiveDocumentContext()$path)) #In scripts folder
-setwd('../') #Goes back to home project directory
+#wd <- c("H:/EJ_wells") # Danae's WD
+wd <- c("G:/Shared drives/emlab/projects/current-projects/calepa-cn/data/health/processed/DataFiles_EmissionsFactors") #Vincent's WD
+setwd(wd)
 getwd()
-
 
 ## Read raw emission factors from Jaramillo & Muller (2016)
 
-ef_raw <- read.csv("./data/emission_factors.csv", stringsAsFactors = FALSE)%>%
+ef_raw <- read.csv("./emission_factors.csv", stringsAsFactors = FALSE)%>%
   filter(source %in% "jaramillo")%>%
   select(-source)
 
@@ -38,4 +38,8 @@ ef <- ef_raw %>%
   mutate(quantity_kg_mmillionbbl = quantity * 1000,
          quantity_kg_bbl = quantity_kg_mmillionbbl/1000000)%>%
   select(-unit); ef
+
+## Save final emission factors (kg/bbl)
+
+write.csv(ef %>% select(-quantity:-quantity_kg_mmillionbbl),"./emission_factors_final.csv",row.names = F)
   
