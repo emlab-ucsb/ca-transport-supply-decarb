701b28f132380ae5ebab188328715d24c65b104c
traceybit <traceymangin@gmail.com>
2021-08-27 17:42:11 -0700

    prep excise tax and carbon for new scens

--- energy/data-processing-prep/prep_excise_tax_scens.R
+++ energy/data-processing-prep/prep_excise_tax_scens.R
@@ -9,22 +9,34 @@ library(rebus)
 ## paths
 scen_path  = '/Volumes/GoogleDrive/Shared drives/emlab/projects/current-projects/calepa-cn/project-materials/scenario-inputs/'
 
-setback_tax_out <- fread(paste0(scen_path, "setback_tax_values.csv"))
+## create full set for finding new scenarios
 
+excise_tax_df <- expand.grid(year = c(2020:2045),
+                             tax_rate = c(0, 0.10, 0.50, 0.90, 1.00, seq(0.01, 0.02, 0.001), seq(0.05, 0.06, 0.001), seq(0.26, 0.27, 0.001),
+                                          seq(0.70, 0.80, 0.001)))
+
+excise_tax_df2 <- excise_tax_df %>%
+  mutate(excise_tax_scenario = paste0("tax_", tax_rate),
+         excise_tax_scenario = ifelse(excise_tax_scenario == "tax_0", "no tax", excise_tax_scenario)) %>%
+  mutate(units = "fraction of oil price")
+
+fwrite(excise_tax_df2, paste0(scen_path, 'all_excise_tax_scenarios.csv'))
+
+
+## final set
+## ----------------------------------------------
+
+setback_tax_out <- fread(paste0(scen_path, "setback_tax_values.csv"))
 
 excise_tax_df <- expand.grid(year = c(2020:2045),
                              tax_rate = c(0, 0.05, 0.10, 0.50, 0.90, 1.00, as.numeric(str_remove(setback_tax_out[, excise_tax_scenario], "tax_"))))
 
 rename_df <- setback_tax_out[, excise_tax_scenario, match_scen]
 rename_df[, new_name := paste0('tax_', match_scen)]
-rename_df[, excise_tax_scenario := str_remove(excise_tax_scenario, "0" %R% DOT)]
+# rename_df[, excise_tax_scenario := str_remove(excise_tax_scenario, "0" %R% DOT)]
 
 excise_tax_df2 <- excise_tax_df %>%
   mutate(excise_tax_scenario = paste0("tax_", tax_rate)) %>%
-  mutate(excise_tax_scenario = str_remove(excise_tax_scenario, "0" %R% DOT),
-         excise_tax_scenario = ifelse(excise_tax_scenario %in% c('tax_5', 'tax_9'), paste0(excise_tax_scenario, "0"),
-                                      ifelse(tax_rate == 1.000, 'tax_100', 
-                                             ifelse(tax_rate == 0.100, 'tax_10', excise_tax_scenario)))) %>%
   left_join(rename_df) %>%
   mutate(excise_tax_scenario = ifelse(excise_tax_scenario == "tax_0", "no tax",
                                       ifelse(is.na(match_scen) & excise_tax_scenario != "tax_0", excise_tax_scenario, new_name))) %>%
@@ -33,7 +45,11 @@ excise_tax_df2 <- excise_tax_df %>%
 
 fwrite(excise_tax_df2, paste0(scen_path, 'final_excise_tax_scenarios.csv'))
 
+## now just include "new" scens
 
+excise_tax_df3 <- excise_tax_df2 %>%
+  filter(excise_tax_scenario %in% c("no tax", paste("tax", setback_tax_out$match_scen, sep = "_")))
 
-# fwrite(excise_tax_df, paste0(scen_path, 'all_excise_tax_scenarios.csv'))
+fwrite(excise_tax_df3, paste0(scen_path, 'comparison_excise_tax_scenarios.csv'))
+
 
