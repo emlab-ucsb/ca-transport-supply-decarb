d4d96b32dc30aaba809d43f5528cb729126292e2
traceybit <traceymangin@gmail.com>
2021-10-25 14:24:45 -0700

    udpate refining subset script

--- energy/compile-outputs/compile_subset_ref.R
+++ energy/compile-outputs/compile_subset_ref.R
@@ -10,7 +10,7 @@ library(openxlsx)
 
 # paths -----
 outputs_path      = '/Volumes/GoogleDrive/Shared drives/emlab/projects/current-projects/calepa-cn/outputs'
-refining_out_path = 'predict-production/refining_2021-09-06/CUF0.6/outputs'
+refining_out_path = 'predict-production/refining_2021-10-22/CUF0.6/outputs'
 
 ## file names
 refining_file     = 'refining_scenario_outputs_refinery_net_exports_revised.csv'
@@ -41,11 +41,11 @@ refining_scens[, BAU_scen := fifelse((oil_price_scenario == 'reference case' &
                                         demand_scenario == 'BAU' &
                                         refining_scenario == 'historic exports'), 1, 0)]
 
-# ## find scen selection
-# carbon_subset_vec <- c("price floor", "price ceiling", "central SCC")
-# carbon_scens_vec <- c("carbon_setback_1000ft", "carbon_setback_5280ft", "carbon_90_perc_reduction", "central SCC")
-# ccs_subset_vec <- c("medium CCS cost", "high CCS cost", "medium CCS cost - 45Q - LCFS", "high CCS cost - 45Q - LCFS")
-# tax_subset_vec <- c("tax_setback_1000ft", "tax_setback_2500ft", "tax_setback_5280ft", "tax_90_perc_reduction")
+## find scen selection
+carbon_subset_vec <- c("price floor", "price ceiling", "central SCC")
+carbon_scens_vec <- c("carbon_setback_1000ft", "carbon_setback_5280ft", "carbon_90_perc_reduction", "central SCC")
+ccs_subset_vec <- c("no ccs", "medium CCS cost", "high CCS cost", "medium CCS cost - 45Q - LCFS", "high CCS cost - 45Q - LCFS")
+tax_subset_vec <- c("tax_setback_1000ft", "tax_setback_2500ft", "tax_setback_5280ft", "tax_90_perc_reduction")
 
 ## find subset scenarios
 ref_carbon_dt = refining_scens[(innovation_scenario == 'low innovation' &
