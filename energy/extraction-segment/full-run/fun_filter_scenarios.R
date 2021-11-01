af3c7b814e0909a734155c4b1ddf31b319fde58e
traceybit <traceymangin@gmail.com>
2021-11-01 12:26:32 -0700

    change selections for no ccs

--- energy/extraction-segment/full-run/fun_filter_scenarios.R
+++ energy/extraction-segment/full-run/fun_filter_scenarios.R
@@ -12,19 +12,37 @@ filter_run_scens = function(scenario_selection, scens, scen_list) {
     sel_scenarios_dt = sel_scenarios_dt[(oil_price_scenario == 'reference case' &
                                            innovation_scenario == 'low innovation' &
                                            carbon_price_scenario == 'price floor' &
-                                           ccs_scenario == 'medium CCS cost' &
+                                           ccs_scenario %in% c('medium CCS cost', 'no ccs') &
                                            # excise_tax_scenario == 'no tax' & ## all tax
                                            setback_scenario == 'no_setback' &
                                            prod_quota_scenario == 'no quota') |
                                           (oil_price_scenario == 'reference case' &
                                            innovation_scenario == 'low innovation' &
-                                           carbon_price_scenario == 'price floor' &
-                                           ccs_scenario == 'medium CCS cost' &
+                                           ccs_scenario %in% c('medium CCS cost', 'no ccs') &
                                            excise_tax_scenario == 'no tax' &
                                            # setback_scenario == 'no_setback' & ## all setback
                                            prod_quota_scenario == 'no quota')]
   }
 
+  
+  if (scenario_selection == 'carbon_scens') {
+    
+    sel_scenarios_dt = sel_scenarios_dt[(oil_price_scenario == 'reference case' &
+                                           innovation_scenario == 'low innovation' &
+                                           # carbon_price_scenario == 'price floor' &
+                                           ccs_scenario %in% c('medium CCS cost', 'no ccs') &
+                                           excise_tax_scenario == 'no tax' & ## all tax
+                                           setback_scenario == 'no_setback' &
+                                           prod_quota_scenario == 'no quota') |
+                                          (oil_price_scenario == 'reference case' &
+                                           innovation_scenario == 'low innovation' &
+                                           carbon_price_scenario == 'price floor' &
+                                           ccs_scenario %in% c('medium CCS cost', 'no ccs') &
+                                           excise_tax_scenario == 'no tax' &
+                                           # setback_scenario == 'no_setback' & ## all setback
+                                           prod_quota_scenario == 'no quota')]
+    
+  }
   
   
   # keep diagnostics only (if that is input) ------
