12fbdebf68363947de39616ec88e08629cb9a188
traceybit <traceymangin@gmail.com>
2021-12-03 12:06:25 -0800

    create input df with all carbon px scens (includeing carbon px + setback to 90% decrease)

--- energy/data-processing-prep/prep_carbon_price_scens.R
+++ energy/data-processing-prep/prep_carbon_price_scens.R
@@ -15,8 +15,9 @@ carbon_file       = 'carbon_prices_revised.csv'
 ## read
 carbon_px_scens <- fread(file.path(scen_path, carbon_file))
 
-## filter for:
-## price floor, central price, & price ceiling
+## -----------------------------------------------------------------------
+## for creating carbon tax pathways for targets/grid search
+## -----------------------------------------------------------------------
 
 carbon_px_scens_filt <- carbon_px_scens[year == 2020]
 
@@ -24,9 +25,10 @@ carbon_px_scens_filt <- carbon_px_scens[year == 2020]
 # start_vals <- c(21.1, 24.18, 25.61, 239.80,
 #                 28.7, 83.36, 283.25)
 
-start_vals <- c(seq(253.2, 253.3, 0.01), seq(213, 214, 0.1),
-                seq(211, 212, 0.1), seq(176, 177, 0.1),
-                seq(105, 110, 0.05), 73.01)
+
+start_vals <- c(253.22, seq(213.6, 213.7, 0.01),
+                seq(211.1, 211.2, 0.01), seq(176.8, 176.9, 0.01),
+                seq(107.25, 107.3, 0.01), 73.01)
 
 ## carbon px setback/ 90 perc scens
 
@@ -84,7 +86,7 @@ fwrite(carbon_px_scens_search, paste0(scen_path, '/carbon_px_scens_search.csv'))
 
 setback_carbon_out <- fread(paste0(scen_path, "/setback_carbon_values.csv"))
 
-start_vals <- setback_carbon_out[, .(carbon_price_scenario, target_scen, ccs_scenario)]
+start_vals <- setback_carbon_out[, .(carbon_price_scenario, setback_scenario, target_scen, ccs_scenario)]
 start_vals[, carbon_price_scenario := as.numeric(sub("px", "", carbon_price_scenario))]
  
 ## carbon px setback/ 90 perc scens
@@ -94,17 +96,20 @@ carbon_px_scens_search[, units := 'dollars per metric ton CO2']
 setnames(carbon_px_scens_search, 'carbon_price_scenario', 'carbon_price')
 
 carbon_px_scens_search[, carbon_price_scenario := paste0("carbon_", target_scen)]
-setcolorder(carbon_px_scens_search, c("year", 'ccs_scenario', "carbon_price_scenario", "carbon_price", "units"))
+setcolorder(carbon_px_scens_search, c("year", "ccs_scenario", "setback_scenario", "carbon_price_scenario", "carbon_price", "units"))
 
 carbon_px_scens_search[, target_scen := NULL]
 
+## change carbon price scenario for setback-carbon px scens
+carbon_px_scens_search[, carbon_price_scenario := fifelse(carbon_price_scenario == 'carbon_90_perc_reduction' &
+                                                            setback_scenario != 'no_setback', 'carbon_sb_90_perc_reduction', carbon_price_scenario)]
+
 ## add id
-carbon_px_scens_search[, id := paste0(carbon_price_scenario, "-", ccs_scenario)]
+carbon_px_scens_search[, id := paste0(carbon_price_scenario, "-", setback_scenario, '-', ccs_scenario)]
 
-## add 
+## create annual values
+## --------------------------------
 
-
-##
 carbon_px_scens_search2 <- expand.grid(year = 2020:2045,
                                        id = unique(carbon_px_scens_search[, id]))
 
@@ -136,17 +141,18 @@ calculate_val <- function(x0, r, t) {
 carbon_px_scens_search <- carbon_px_scens_search %>%
   mutate(carbon_price_scenario = as.character(carbon_price_scenario),
          units = as.character(units)) %>%
-  group_by(ccs_scenario, carbon_price_scenario) %>%
+  fill(setback_scenario) %>%
+  group_by(ccs_scenario, setback_scenario, carbon_price_scenario) %>%
   mutate(tval = row_number() - 1) %>%
   fill(carbon_price) %>%
   ungroup() %>%
   mutate(carbon_price = ifelse(tval == 0, carbon_price, calculate_val(x0 = carbon_price, r = perc_inc, t = tval))) %>%
-  select(year, ccs_scenario, carbon_price_scenario, carbon_price, units)
+  select(year, ccs_scenario, setback_scenario, carbon_price_scenario, carbon_price, units)
 
 ## cobmine ccs_scenario with carbon price scenario
 carbon_px_scens_search <- carbon_px_scens_search %>%
-  mutate(carbon_price_scenario = paste(carbon_price_scenario, ccs_scenario, sep = "-")) %>%
-  select(-ccs_scenario)
+  mutate(carbon_price_scenario = paste(carbon_price_scenario, setback_scenario, ccs_scenario, sep = "-")) %>%
+  select(-ccs_scenario, -setback_scenario)
 
 ## bind with other scenarios
 carbon_px_scens_search <- rbind(carbon_px_scens_search, carbon_px_scens)
