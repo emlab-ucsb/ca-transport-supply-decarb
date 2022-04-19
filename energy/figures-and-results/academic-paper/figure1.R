d30ac1b2b3336fc612088ef24541a48e4ae98a37
Tracey Mangin <traceymangin@Traceys-MacBook-Pro.local>
2022-04-19 10:00:02 -0700

    work on danae's fig

--- energy/figures-and-results/academic-paper/figure1.R
+++ energy/figures-and-results/academic-paper/figure1.R
@@ -535,36 +535,11 @@ embed_fonts(paste0(main_path, fig_path, 'figs/main-text-revisions/figure1_cd.pdf
 ## recreate Danae's figure
 ## -----------------------------------------------------------
 
-#Danae Hernandez-Cortes hernandezcortes@ucsb.edu
-#INFRASTRUCTURE TO SEND TO THE MODELING TEAM
-#Libraries
-library(sf)
-library(tidyverse)
-library(ggplot2)
-library(rgdal)
-library(maptools)
-library(raster)
-library(rgeos)
-library(plyr)
-library(reshape2)
-library(purrr)
-library(foreign)
-library(haven)
-library(readr)
-library(dplyr)
-library(maps)
-rm(list=ls())
-
-
-
-#############################################
-# PREPARE FILES FROM INMAP OUTPUT: EXTRACTION
-#############################################
-
 #Census tracts
-CA_ct <- st_read(paste0(main_path, "data/GIS/raw/census-tract/tl_2019_06_tract.shp")) 
+CA_ct <- st_read(paste0(main_path, "data/GIS/raw/census-tract/tl_2019_06_tract.shp")) %>%
+  st_transform(ca_crs)
 
-#Torrance refinery
+#Site vector
 sites_vector <- c(1)
 
 read_extraction <- function(buff_site){
@@ -589,14 +564,20 @@ read_extraction <- function(buff_site){
 
 #DO THE FUNCTION
 extraction_srm <- map_df(sites_vector, read_extraction) %>% bind_rows()
-extraction_srm <- dplyr::rename(extraction_srm,weighted_totalpm25 = totalpm25_aw)
+extraction_srm <- dplyr::rename(extraction_srm, weighted_totalpm25 = totalpm25_aw)
 
 extraction_srm_reshape <- dcast(extraction_srm, site + GEOID ~ poll, value.var = "weighted_totalpm25")
-srm_all_pollutants_extraction <- dplyr::rename(extraction_srm_reshape, weighted_totalpm25nh3 = nh3, weighted_totalpm25nox = nox, weighted_totalpm25pm25 = pm25, weighted_totalpm25sox = sox, weighted_totalpm25voc = voc, site_id = site)
+srm_all_pollutants_extraction <- dplyr::rename(extraction_srm_reshape, 
+                                               weighted_totalpm25nh3 = nh3, 
+                                               weighted_totalpm25nox = nox, 
+                                               weighted_totalpm25pm25 = pm25, 
+                                               weighted_totalpm25sox = sox, 
+                                               weighted_totalpm25voc = voc, 
+                                               site_id = site)
 
 srm_all_pollutants_extraction$total_pm25 = srm_all_pollutants_extraction$weighted_totalpm25nh3 + srm_all_pollutants_extraction$weighted_totalpm25nox + srm_all_pollutants_extraction$weighted_totalpm25pm25 + srm_all_pollutants_extraction$weighted_totalpm25sox + srm_all_pollutants_extraction$weighted_totalpm25voc
 
-map <- left_join(CA_ct, srm_all_pollutants_extraction, by = c("GEOID"))
+ct_map <- left_join(CA_ct, srm_all_pollutants_extraction, by = c("GEOID"))
 
 ##DACS
 
@@ -604,49 +585,54 @@ dac_population <- read.csv(paste0(main_path, "data/health/raw/ces3results_part.c
   subset(sb535_dac=="Yes")%>%
   dplyr::rename(GEOID=census_tract)
 
-CA_ct$GEOID=as.double(CA_ct$GEOID)
+CA_ct$GEOID = as.double(CA_ct$GEOID)
 
-dac_map<-left_join(CA_ct,dac_population, by=c("GEOID"))
-dac_map<-dac_map%>%dplyr::filter(sb535_dac=="Yes" & COUNTYFP=="037")
+dac_map <- left_join(CA_ct, dac_population, by=c("GEOID"))
+dac_map <- dac_map%>%dplyr::filter(sb535_dac=="Yes" & COUNTYFP=="037")
 
-CA_counties<-st_read(paste0(main_path, "data/GIS/raw/CA_counties_noislands/CA_Counties_TIGER2016_noislands.shp"))
+## counties
+CA_counties<-st_read(paste0(main_path, "data/GIS/raw/CA_counties_noislands/CA_Counties_TIGER2016_noislands.shp")) %>%
+  st_transform(ca_crs)
 
-LAcontour<-CA_counties%>%subset(COUNTYFP=='037')
-# SFcontour<-CA_counties%>%subset(COUNTYFP=='013'|COUNTYFP=='001'|COUNTYFP=="081"| COUNTY=="075")
+LAcontour <- CA_counties %>% subset(COUNTYFP=='037')
 
-LA_contour_cropped<-st_crop(LAcontour, xmin=-119.4, xmax=117.46, ymin=32, ymax=34.84)
+LA_contour_cropped <- st_crop(LAcontour, xmin = -119.4, xmax = 117.46, ymin = 32, ymax = 34.84)
+
+## extraction clusters
+extraction_clusters <- st_read(paste0(main_path, "outputs/academic-out/extraction/extraction_fields_clusters_10km.shp")) %>%
+  st_transform(ca_crs)
+
+field1 <- extraction_clusters %>% dplyr::filter(OBJECTID==1)
+
+LA_contour_cropped <- st_crop(LAcontour, xmin = -118.9, xmax = -118.46, ymin = 32, ymax = 34.84)
+
+## LA metro
+LA_metro <- st_read(paste0(main_path, "data/GIS/raw/OnlyLA_Urban_Areas/U.S._Census_Urbanized_Areas_%E2%80%93_SCAG_Region.shp")) %>%
+  st_transform(ca_crs)
+
+LA_metro <- LA_metro %>% dplyr::filter(NAME10 == "Los Angeles--Long Beach--Anaheim, CA")
+
+# field1wgs84<-st_transform(field1,4326)
+# field1_intersection <- st_intersection(LA_metro, field1wgs84)
+
+field1_intersection <- st_intersection(LA_metro, field1)
 
 
-extraction_clusters <- st_read(paste0(main_path, "outputs/academic-out/extraction/extraction_fields_clusters_10km.shp"))
-field1 <- extraction_clusters%>%dplyr::filter(OBJECTID==1)
-# torrance<-st_read("C:/Users/dhern125/Dropbox/UCSB-PhD/emLab/CALEPA/CALEPA_bren_computer/CALEPA/GIS/buffers/torrance_refinery.shp")
 
-LA_contour_cropped<-st_crop(LAcontour, xmin=-118.9, xmax=-118.46, ymin=32, ymax=34.84)
+LA <- ct_map %>% subset(COUNTYFP=='037') %>% dplyr::filter(AWATER < ALAND)
+# LA1 <- st_transform(LA, 4326)
+LA2 <- st_intersection(LA_metro, LA)
 
-LA_metro <- st_read(paste0(main_path, "data/GIS/raw/OnlyLA_Urban_Areas/U.S._Census_Urbanized_Areas_%E2%80%93_SCAG_Region.shp"))
-LA_metro <- LA_metro%>%dplyr::filter(NAME10=="Los Angeles--Long Beach--Anaheim, CA")
-
-field1wgs84<-st_transform(field1,4326)
-
-field1_intersection <- st_intersection(LA_metro, field1wgs84)
-
-
-
-
-LA <- map%>%subset(COUNTYFP=='037') %>% dplyr::filter(AWATER<ALAND)
-LA1 <- st_transform(LA, 4326)
-LA2 <- st_intersection(LA_metro, LA1)
-
-map2 <- st_transform(map, ca_crs)
+# map2 <- st_transform(ct_map, ca_crs)
 
 ## califonia
 states <- st_as_sf(map("state", plot = FALSE, fill = TRUE))
 
 california <- states %>% filter(ID == "california") %>%
-  st_transform(4269)
+  st_transform(ca_crs)
 
 
-all_SC <- st_intersection(california, map2)
+# all_SC <- st_intersection(california, map2)
 
 
 # total_pm25 <- ggplot(data = LA) +
@@ -715,15 +701,16 @@ disp_win2_trans <- st_transform(disp_win2_wgs84, crs = ca_crs)
 disp_win2_coord <- st_coordinates(disp_win2_trans)
 
 mapRangeLim<-c(-123,-115,33,39)
-map2<-st_transform(map,ca_crs)
+
+# map2 <- st_transform(map_ct,ca_crs)
 field1_intersection2<-st_transform(field1_intersection,ca_crs)
 
 mapRangeLim<-c(-114,-123,32.5,39)
 
 total_pm25 <- ggplot () +
-  geom_sf(data = map, aes(fill=total_pm25), color=NA) + theme_void() + labs(fill=expression(paste("PM"[2.5], " (",mu,"/",m^3,")"))) +
+  geom_sf(data = ct_map, aes(fill=total_pm25), color=NA) + theme_void() + labs(fill=expression(paste("PM"[2.5], " (",mu,"/",m^3,")"))) +
   scale_fill_gradient(high = "#FF0000", low = "#FFFFFF", space = "Lab", 
-                      limits = c(min(map$total_pm25), max(map$total_pm25))) +
+                      limits = c(min(ct_map$total_pm25), max(ct_map$total_pm25))) +
   geom_sf(data = field1_intersection, fill="transparent", color="black") +
   geom_sf(data = california, fill="transparent", color="gray65")+
   coord_sf(xlim = mapRangeLim[c(1:2)], ylim = mapRangeLim[c(3:4)])+
