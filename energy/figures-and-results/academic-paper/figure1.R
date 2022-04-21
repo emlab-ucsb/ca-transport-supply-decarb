05128614e67d25c3b0c8d42025ac0e7c6e9ea934
traceybit <traceymangin@gmail.com>
2022-04-21 15:39:32 -0700

    update fig 1

--- energy/figures-and-results/academic-paper/figure1.R
+++ energy/figures-and-results/academic-paper/figure1.R
@@ -298,7 +298,7 @@ dac_legend <- get_legend(
 
 ## plot together
 map_fig_a <- ggdraw(fig1_map) +
-  draw_plot(dac_legend, x = 0.2, y = 0.2, width = 0.05, height = 0.05) +
+  draw_plot(dac_legend, x = 0.1, y = 0.25, width = 0.03, height = 0.03) +
   draw_plot(fig1_inset, x = 0.65, y = 0.65, width = 0.3, height = 0.3) +
   draw_plot_label(
     c("A", ""),
@@ -332,16 +332,6 @@ embed_fonts(paste0(main_path, fig_path, 'figs/main-text-revisions/figure1a.pdf')
 pred_wells_fm <- read_csv(paste0(data_path, "new_wells_pred_revised.csv")) %>%
   mutate(doc_field_code = paste0("00", doc_field_code),
          doc_field_code = as.character(str_sub(doc_field_code, start= -3))) 
-
-# pred_wells_all <- pred_wells_fm %>%
-#   rename(new_wells_pred_fm = new_wells_pred) %>%
-#   left_join(pred_wells_ho) %>%
-#   rename(new_wells_pred_ho = new_wells_pred) %>%
-#   select(doc_field_code, year, new_wells, new_wells_pred_ho, new_wells_pred_fm) %>%
-#   pivot_longer(new_wells:new_wells_pred_fm, names_to = "category", values_to = "n_wells") %>%
-#   mutate(label_name = ifelse(category == "new_wells", "Observed entry",
-#                              ifelse(category == "new_wells_pred_ho", "Predicted entry", "Predicted entry")))
-
 
 # load historic production
 well_prod_19 <- well_prod[year == 2019]
@@ -381,7 +371,7 @@ state_df <- well_n_df_revised %>%
 ## state no hold out
 state_fig_fs <- 
   ggplot(state_df %>% filter(category != "new_wells_pred_ho"), aes(x = year, y = n_wells, color = label_name)) +
-  geom_line(size = 0.6, alpha = 0.8) +
+  geom_line(size = 1, alpha = 0.8) +
   labs(y = "Number of wells",
        x = NULL,
        color = NULL) +
@@ -519,6 +509,7 @@ ct_2019 <- census_tracts %>%
 
 ## blue color pal
 blues_pal <- c("#FAFAFA", "#778DA9", "#415A77", "#1B263B", "#0D1B2A")
+red_pal <- c("#FAFAFA", "#A84268")
 
 
 # ## crop area
@@ -530,11 +521,13 @@ blues_pal <- c("#FAFAFA", "#778DA9", "#415A77", "#1B263B", "#0D1B2A")
 # disp_win2_coord <- st_coordinates(disp_win2_trans)
 
 
-ct_map <- ggplot() +
+ct_health_map <- ggplot() +
   # geom_sf(data = california, mapping = aes(), fill = "white", lwd = 0.4, show.legend = FALSE) +
   # geom_sf(data = california, mapping = aes(), fill = "white", lwd = 0.4, show.legend = FALSE) +
   # geom_sf(data = dac_areas , mapping = aes(geometry = geometry), fill = "#9DBF9E", lwd = 0, color = "white", show.legend = TRUE) +
   geom_sf(data = ct_2019, mapping = aes(geometry = geometry, fill = pop_x_pm25), lwd = 0.0, color = "white", alpha = 1, show.legend = TRUE) +
+  scale_fill_gradient(high = "#A84268", low = "#FAFAFA", space = "Lab", na.value = "grey50",
+                      limits = c(min(ct_2019$pop_x_pm25), max(ct_2019$pop_x_pm25))) +
   # geom_sf(data = county_19, mapping = aes(geometry = geometry), fill = NA, color = "#4A6C6F", lwd = 0.5) +
   geom_sf(data = CA_counties, mapping = aes(geometry = geometry), lwd = 0.15, alpha = 0) +
   geom_sf_text(data = county_19, mapping = aes(geometry = geometry, label = adj_county_name), size = 2, fontface = "bold", color = "black") +
@@ -543,7 +536,7 @@ ct_map <- ggplot() +
        fill = 'Population * PM2.5',
        x = NULL,
        y = NULL) +
-  scale_fill_gradientn(colors = blues_pal) +
+  # scale_fill_gradientn(colors = blues_pal) +
   coord_sf(xlim = disp_win2_coord[,'X'], ylim = disp_win2_coord[,'Y'],
            datum = ca_crs, expand = FALSE) +
   # geom_sf_text(data = all_county_prod_df %>% filter(metric == 'difference (bbls)', scenario == name), aes(geometry = geometry, label = paste0(adj_county_name, '\n ', round(adj_val, digits = 2), ' mbbls')), colour = "black", size = 2) +
@@ -611,7 +604,7 @@ labor_map <- ggplot() +
 
 ## plot together
 maps2 <- plot_grid(
-  ct_map,
+  ct_health_map,
   labor_map,
   align = 'v',
   # labels = c("C", "D"),
@@ -623,13 +616,13 @@ maps2 <- plot_grid(
 
 ggsave(maps2,
        filename = file.path(main_path, fig_path, 'figs/main-text-revisions/figure1_cd.png'),
-       width = 6,
+       width = 7,
        height = 3.5,
        units = "in")
 
 ggsave(maps2,
        filename = file.path(main_path, fig_path, 'figs/main-text-revisions/figure1_cd.pdf'),
-       width = 6,
+       width = 7,
        height = 3.5,
        units = "in",
        device = 'pdf')
@@ -725,51 +718,53 @@ ct_map_county <- ct_map %>%
 
 ## crop
 ## -----------------------------------
-disp_win_la_wgs84 <- st_sfc(st_point(c(-119.4, 32)), st_point(c(-117.46, 34.84)),
+disp_win_la_wgs84 <- st_sfc(st_point(c(-119.4, 33.2)), st_point(c(-117, 34.84)),
                           crs = 4326)
 
 disp_win_la_trans <- st_transform(disp_win_la_wgs84, crs = ca_crs)
 
 disp_win_la_coord <- st_coordinates(disp_win_la_trans)
 
+zoom_coord_df <- as.data.frame(disp_win_la_coord)
 
 
-ct_cropped <- st_crop(ct_map_county, xmin = 56832.65, xmax = 232132.27, ymin = -667547.6, ymax = -349842.0)
-county_crop <- st_crop(CA_counties, xmin = 56832.65, xmax = 232132.27, ymin = -667547.6, ymax = -349842.0)
+ct_cropped <- st_crop(ct_map_county, xmin = zoom_coord_df$X[1], xmax = zoom_coord_df$X[2], ymin = zoom_coord_df$Y[1], ymax = zoom_coord_df$Y[2])
+county_crop <- st_crop(CA_counties, xmin = zoom_coord_df$X[1], xmax = zoom_coord_df$X[2], ymin = zoom_coord_df$Y[1], ymax = zoom_coord_df$Y[2])
 
 
 ## counties
 # CA_counties<-st_read(paste0(main_path, "data/GIS/raw/CA_counties_noislands/CA_Counties_TIGER2016_noislands.shp")) %>%
 #   st_transform(ca_crs)
 
-LAcontour <- CA_counties %>% subset(COUNTYFP=='037')
-
-LA_contour_cropped <- st_crop(LAcontour, xmin = -119.4, xmax = 117.46, ymin = 32, ymax = 34.84)
+# LAcontour <- CA_counties %>% subset(COUNTYFP=='037')
+# 
+# LA_contour_cropped <- st_crop(LAcontour, xmin = -119.4, xmax = 117.46, ymin = 32, ymax = 34.84)
 
 ## extraction clusters
 extraction_clusters <- st_read(paste0(main_path, "outputs/academic-out/extraction/extraction_fields_clusters_10km.shp")) %>%
   st_transform(ca_crs)
 
-field1 <- extraction_clusters %>% dplyr::filter(OBJECTID==1)
+field1 <- extraction_clusters %>% dplyr::filter(OBJECTID==1) %>%
+  mutate(name = "Field cluster")
 
-LA_contour_cropped <- st_crop(LAcontour, xmin = -118.9, xmax = -118.46, ymin = 32, ymax = 34.84)
+# LA_contour_cropped <- st_crop(LAcontour, xmin = -118.9, xmax = -118.46, ymin = 32, ymax = 34.84)
 
 ## LA metro
-LA_metro <- st_read(paste0(main_path, "data/GIS/raw/OnlyLA_Urban_Areas/U.S._Census_Urbanized_Areas_%E2%80%93_SCAG_Region.shp")) %>%
-  st_transform(ca_crs)
-
-LA_metro <- LA_metro %>% dplyr::filter(NAME10 == "Los Angeles--Long Beach--Anaheim, CA")
+# LA_metro <- st_read(paste0(main_path, "data/GIS/raw/OnlyLA_Urban_Areas/U.S._Census_Urbanized_Areas_%E2%80%93_SCAG_Region.shp")) %>%
+#   st_transform(ca_crs)
+# 
+# LA_metro <- LA_metro %>% dplyr::filter(NAME10 == "Los Angeles--Long Beach--Anaheim, CA")
 
 # field1wgs84<-st_transform(field1,4326)
 # field1_intersection <- st_intersection(LA_metro, field1wgs84)
 
-field1_intersection <- st_intersection(LA_metro, field1)
+# field1_intersection <- st_intersection(LA_metro, field1)
 
 
 
-LA <- ct_map %>% subset(COUNTYFP=='037') %>% dplyr::filter(AWATER < ALAND)
-# LA1 <- st_transform(LA, 4326)
-LA2 <- st_intersection(LA_metro, LA)
+# LA <- ct_map %>% subset(COUNTYFP=='037') %>% dplyr::filter(AWATER < ALAND)
+# # LA1 <- st_transform(LA, 4326)
+# LA2 <- st_intersection(LA_metro, LA)
 
 # map2 <- st_transform(ct_map, ca_crs)
 
@@ -796,137 +791,87 @@ LA2 <- st_intersection(LA_metro, LA)
 # plot(total_pm25)
 
 ## total pm2.5
-zoom_c <- c('Los Angeles', 'Ventura', 'Orange', 'San Bernardino', 'Kern')
+# zoom_c <- c('Los Angeles', 'Ventura', 'Orange', 'San Bernardino', 'Kern')
 
 total_pm25 <- ggplot() +
   geom_sf(data = ct_cropped, aes(fill=total_pm25), color=NA) + theme_void() + labs(fill=expression(paste("PM"[2.5], " (",mu,"/",m^3,")"))) +
-  scale_fill_gradient(high = "#A84268", low = "#FFFFFF", space = "Lab", na.value = "grey50",
+  scale_fill_gradient(high = "#A84268", low = "#FAFAFA", space = "Lab", na.value = "grey50",
                       limits = c(min(ct_cropped$total_pm25), max(ct_cropped$total_pm25))) +
   # geom_sf(data = california, mapping = aes(), fill = "#FAFAFA", lwd = 0.4, show.legend = FALSE) +
   geom_sf(data = county_crop, mapping = aes(geometry = geometry), lwd = 0.15, alpha = 0) +
   geom_sf_text(data = CA_counties %>% filter(NAME %in% c("Los Angeles", "Orange", "Ventura")), mapping = aes(geometry = geometry, label = NAME), size = 2, fontface = "bold", color = "black") +
   # geom_sf(data = dac_map, fill="transparent", color="gray65", size = 0.1) +
   # geom_sf(data = LA_metro, fill="transparent", color="gray65") +
-  geom_sf(data = field1, fill="transparent", color="black")
-
-  
-  
-  total_pm25 <- ggplot (data = LA) +
-  geom_sf(data = LA, aes(fill=total_pm25), color=NA) + theme_void() + labs(fill=expression(paste("PM"[2.5], " (",mu,"/",m^3,")"))) +
-  scale_fill_gradient(high = "#FF0000", low = "#FFFFFF", space = "Lab", na.value = "gray50",
-                      limits = c(min(LA$total_pm25), max(LA$total_pm25))) +
-  geom_sf(data = dac_map, fill="transparent", color="gray65", size = 0.1) +
-  #geom_sf(data = LAcontour, fill="transparent", color="gray65") +
-  #geom_sf(data = LA_contour_cropped, fill="transparent", color="gray65") +
-  geom_sf(data = LA_metro, fill="transparent", color="gray65") +
-  geom_sf(data = field1_intersection, fill="transparent", color="black") 
-
-pdf("C:/Users/dhern125/Documents/GitHub/ca-transport-supply-decarb/health/outputs/LA_field_cluster_1pop.pdf")
-print(total_pm25)
-dev.off() 
-plot(total_pm25)
-
-
-# ggplot(data = dac_map, fill="transparent", color="black",   size = 0.005) + geom_sf()
-
-
-
-# total_pm25<-ggplot (data = LA) +
-#   geom_sf(data = LA2, aes(fill=total_pm25), color=NA) + theme_void() + labs(fill=expression(paste("PM"[2.5], " (",mu,"/",m^3,")"))) +
-#   scale_fill_gradient(high = "#FF0000", low = "#FFFFFF", space = "Lab", na.value = "gray50",
-#                       limits = c(min(LA$total_pm25), max(LA$total_pm25))) +
-#   #geom_sf(data = LAcontour, fill="transparent", color="gray65") +
-#   geom_sf(data = LA_metro, fill="transparent", color="gray65")+
-#   geom_sf(data = dac_map, fill="transparent", color="gray65",   size = 0.005)+
-#   geom_sf(data = field1_intersection, fill="transparent", color="black") +
-#   coord_sf(xlim = disp_win2_coord[,'X'], ylim = disp_win2_coord[,'Y'],
-#            datum = ca_crs, expand = FALSE) 
-
-pdf("C:/Users/dhern125/Documents/GitHub/ca-transport-supply-decarb/health/outputs/LA_field_cluster_1pop.pdf")
-print(total_pm25)
-dev.off() 
-plot(total_pm25)
-
-
-
-
-
-## counties boundaries
-county_boundaries <- st_read(paste0(main_path, "data/GIS/raw/CA_Counties/CA_Counties_TIGER2016.shp")) %>% 
-  st_transform(ca_crs) %>%
-  dplyr::select(adj_county_name = NAME)
-
-
-
-## crop area
-disp_win2_wgs84 <- st_sfc(st_point(c(-123, 33)), st_point(c(-115, 39)),
-                          crs = 4326)
-
-disp_win2_trans <- st_transform(disp_win2_wgs84, crs = ca_crs)
-
-disp_win2_coord <- st_coordinates(disp_win2_trans)
-
-mapRangeLim<-c(-123,-115,33,39)
-
-# map2 <- st_transform(map_ct,ca_crs)
-field1_intersection2<-st_transform(field1_intersection,ca_crs)
-
-mapRangeLim<-c(-114,-123,32.5,39)
-
-total_pm25 <- ggplot () +
-  geom_sf(data = ct_map, aes(fill=total_pm25), color=NA) + theme_void() + labs(fill=expression(paste("PM"[2.5], " (",mu,"/",m^3,")"))) +
-  scale_fill_gradient(high = "#FF0000", low = "#FFFFFF", space = "Lab", 
-                      limits = c(min(ct_map$total_pm25), max(ct_map$total_pm25))) +
-  geom_sf(data = field1_intersection, fill="transparent", color="black") +
-  geom_sf(data = california, fill="transparent", color="gray65")+
-  coord_sf(xlim = mapRangeLim[c(1:2)], ylim = mapRangeLim[c(3:4)])+
+  geom_sf(data = field1, fill="transparent", color="black") +
+  geom_sf_text(data = field1, mapping = aes(geometry = geometry, label = name), 
+               size = 2, fontface = "bold", color = "black", nudge_y = 20000, nudge_x = 10000) +
+  theme_void() +
   theme(
     # legend.justification defines the edge of the legend that the legend.position coordinates refer to
     legend.justification = c(0, 1),
     # Set the legend flush with the left side of the plot, and just slightly below the top of the plot
-    legend.position = c(0.10, 0.15),
-    legend.title = element_text(size = 9)) +
+    legend.position = c(0.12, 0.2),
+    legend.title = element_text(size = 7),
+    plot.title = element_text(size = 11)) +
   guides(fill = guide_colourbar(title.position="top", 
                                 title.hjust = 0,
                                 direction = "horizontal"))
 
-pdf("C:/Users/dhern125/Documents/GitHub/ca-transport-supply-decarb/health/outputs/LA_field_cluster_1pop.pdf")
-print(total_pm25)
-dev.off() 
-plot(total_pm25)
+  
+ggsave(total_pm25,
+       filename = file.path(main_path, fig_path, 'figs/main-text-revisions/fig1_exposure.png'),
+       width = 3.5,
+       height = 3.5,
+       units = "in")
+
+ggsave(total_pm25,
+       filename = file.path(main_path, fig_path, 'figs/main-text-revisions/fig1_exposure.pdf'),
+       width = 3.5,
+       height = 3.5,
+       units = "in",
+       device = 'pdf')
+
+embed_fonts(paste0(main_path, fig_path, 'figs/main-text-revisions/fig1_exposure.pdf'),
+            outfile = paste0(main_path, fig_path, 'figs/main-text-revisions/fig1_exposure.pdf'))
+
+
+## -----------------------------------------------------------
+## arrange all plots together
+## -----------------------------------------------------------
+
+## plot together
+all_fig1 <- plot_grid(
+  map_fig_a,
+  state_fig_fs,
+  labor_map,
+  NULL,
+  total_pm25,
+  ct_health_map,
+  align = 'hv',
+  # labels = c("C", "D"),
+  # label_size = 10,
+  # hjust = -1,
+  nrow = 2,
+  rel_widths = c(1, 1, 1, 1, 1, 1)
+)
+
+ggsave(all_fig1,
+       filename = file.path(main_path, fig_path, 'figs/main-text-revisions/fig1_all.png'),
+       width = 12,
+       height = 7.5,
+       units = "in")
+
+ggsave(all_fig1,
+       filename = file.path(main_path, fig_path, 'figs/main-text-revisions/fig1_all.pdf'),
+       width = 12,
+       height = 7.5,
+       units = "in",
+       device = 'pdf')
+
+embed_fonts(paste0(main_path, fig_path, 'figs/main-text-revisions/fig1_all.pdf'),
+            outfile = paste0(main_path, fig_path, 'figs/main-text-revisions/fig1_all.pdf'))
 
 
 
 
-
-# ct_map <- ggplot() +
-#   geom_sf(data = california, mapping = aes(), fill = "#FFFAF5", lwd = 0.4, show.legend = FALSE) +
-#   # geom_sf(data = california, mapping = aes(), fill = "white", lwd = 0.4, show.legend = FALSE) +
-#   # geom_sf(data = dac_areas , mapping = aes(geometry = geometry), fill = "#9DBF9E", lwd = 0, color = "white", show.legend = TRUE) +
-#   geom_sf(data = ct_2019, mapping = aes(geometry = geometry, fill = pop_x_pm25), lwd = 0, alpha = 1, show.legend = TRUE) +
-#   # geom_sf(data = county_19, mapping = aes(geometry = geometry), fill = NA, color = "#4A6C6F", lwd = 0.5) +
-#   geom_sf_text(data = county_19, mapping = aes(geometry = geometry, label = adj_county_name), size = 2, fontface = "bold", color = "black") +
-#   # scale_fill_gradient2(midpoint = 0, low = "red", mid = "white", high = "blue") +
-#   labs(title = 'Population * PM2.5 by census tract',
-#        fill = 'Population * PM2.5',
-#        x = NULL,
-#        y = NULL) +
-#   scale_fill_gradientn(colors = blues_pal) +
-#   coord_sf(xlim = disp_win2_coord[,'X'], ylim = disp_win2_coord[,'Y'],
-#            datum = ca_crs, expand = FALSE) +
-#   # geom_sf_text(data = all_county_prod_df %>% filter(metric == 'difference (bbls)', scenario == name), aes(geometry = geometry, label = paste0(adj_county_name, '\n ', round(adj_val, digits = 2), ' mbbls')), colour = "black", size = 2) +
-#   theme_void() +
-#   theme(
-#     # legend.justification defines the edge of the legend that the legend.position coordinates refer to
-#     legend.justification = c(0, 1),
-#     # Set the legend flush with the left side of the plot, and just slightly below the top of the plot
-#     legend.position = c(0.10, 0.15),
-#     legend.title = element_text(size = 9)) +
-#   guides(fill = guide_colourbar(title.position="top", 
-#                                 title.hjust = 0,
-#                                 direction = "horizontal"))
-# 
-# 
-# 
-# 
-# 
+  
