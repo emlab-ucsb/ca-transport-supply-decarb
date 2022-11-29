aec49e93772ffff22b4bb110192185674365b6c7
Tracey Mangin <traceymangin@gmail.com>
2022-11-29 00:39:22 -0800

    compare two other files

--- energy/extraction-segment/model-prep/extra/compare_entry_exit.R
+++ energy/extraction-segment/model-prep/extra/compare_entry_exit.R
@@ -15,6 +15,8 @@ orig_nw_file    <- "new_wells_pred_revised.csv"
 orig_entry_file <- "poisson_regression_coefficients_revised.csv" 
 rev_nw_file     <- "new_wells_pred_revised_2211.csv"
 rev_entry_file  <- "poisson_regression_coefficients_revised_2211.csv" 
+entry_file      <- "entry_revised.csv"
+r_entry_file  <- "entry_revised_2211.csv"
 
 ## read in files
 orig_nw <- fread(paste0(model_path, orig_nw_file), header = T, colClasses = c('doc_field_code' = 'character'))
@@ -22,6 +24,9 @@ rev_nw <- fread(paste0(model_path, rev_nw_file), header = T, colClasses = c('doc
 
 orig_entry <- fread(paste0(model_path, orig_entry_file), header = T, colClasses = c('doc_field_code' = 'character'))
 rev_entry <- fread(paste0(model_path, rev_entry_file), header = T, colClasses = c('doc_field_code' = 'character'))
+
+orig_e <- fread(paste0(model_path, "rui-files/", entry_file), header = T, colClasses = c('doc_field_code' = 'character'))
+rev_e <- fread(paste0(model_path, "rui-files/", r_entry_file), header = T, colClasses = c('doc_field_code' = 'character'))
 
 ## pivot longer and merge
 
@@ -37,7 +42,7 @@ new_well_comp <- orig_nw %>%
 
 write_csv(new_well_comp, paste0(save_path, "comp_new_well_pred.csv"))
 
-
+##
 orig_entry <- orig_entry %>%
   pivot_longer(brent_hat:fixed_effect, names_to = "name", values_to = "orig_value")
 
@@ -50,5 +55,25 @@ entry_comp <- orig_entry %>%
 
 write_csv(entry_comp, paste0(save_path, "comp_entry_coefs.csv"))
 
+##
+orig_e <- orig_e %>%
+  pivot_longer(new_wells:field_categ, names_to = "name", values_to = "orig_value")
+
+rev_e <- rev_e %>%
+  pivot_longer(new_wells:field_categ, names_to = "name", values_to = "rev_value")
+
+e_comp <- orig_e %>%
+  left_join(rev_e) %>%
+  mutate(diff = rev_value - orig_value)
+
+## category df
+cat_comp <- e_comp %>%
+  filter(name == "field_categ") %>%
+  select(doc_field_code, doc_fieldname, name, orig_value, value_2211 = rev_value, diff) %>%
+  unique() %>%
+  mutate(diff_value = ifelse(diff != 0, 1, 0)) %>%
+  select(-diff)
+
+write_csv(cat_comp, paste0(save_path, "cat_comp_entry_revised.csv"))
 
 
