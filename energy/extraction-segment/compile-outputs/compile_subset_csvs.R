e2a0860797db4d397deee0745d22f28aebefae51
Tracey Mangin <traceymangin@gmail.com>
2022-12-27 23:42:16 -0500

    recompile outputs and compile outputs for health sensitivity

--- energy/extraction-segment/compile-outputs/compile_subset_csvs.R
+++ energy/extraction-segment/compile-outputs/compile_subset_csvs.R
@@ -5,17 +5,17 @@
 ## libraries
 library(data.table)
 
-external_save <- 1
+external_save <- 0
 
 ## paths
 main_path         <- '/Volumes/GoogleDrive/Shared\ drives/emlab/projects/current-projects/calepa-cn'
-academic_out_path      <- file.path(main_path, 'outputs/academic-out/extraction/')
+academic_out_path <- file.path(main_path, 'outputs/academic-out/extraction/')
 input_path        <- file.path(main_path, 'outputs/academic-out/extraction/nature-energy-rev-outputs/')
 
 ## read in saved rds files - updates as needed
-# extraction_folder = 'extraction_2022-11-07'
+extraction_folder = 'extraction_2022-12-27'
 
-external_path <- '/Volumes/calepa/academic-out/extraction_2022-11-16/'
+external_path <- '/Volumes/calepa/academic-out/extraction_2022-12-27/'
 
 ## get correct path
 
@@ -31,8 +31,10 @@ if(external_save == 1) {
 ## sub folders
 field_path     = paste0(compiled_path, 'field-results/')
 state_path     = paste0(compiled_path, 'state-results/')
+state_hs_path  = paste0(compiled_path, 'state-results/health_sens/')
 county_path    = paste0(compiled_path, 'county-results/')
 ct_path        = paste0(compiled_path, 'census-tract-results/')
+ct_hs_path     = paste0(compiled_path, 'census-tract-county-results/')
 
 
 ## files
@@ -48,9 +50,11 @@ subset_ids <- subset_list[, .(scen_id, target, target_policy)]
 ## start function
 ## 1) read in rds for subset ids; 2) save to drive; 3) compile field level outputs for health (for now)
 
-county_out_list <- list()
-state_out_list <- list()
-ct_out_list <- list()
+county_out_list   <- list()
+state_out_list    <- list()
+state_hs_out_list <- list()
+ct_out_list       <- list()
+ct_hs_out_list    <- list()
 
 for (i in 1:nrow(subset_ids)) {
   
@@ -58,33 +62,54 @@ for (i in 1:nrow(subset_ids)) {
   
   id_name_tmp <- subset_ids[i, scen_id]
   
+  ## census tract (main results)
   ct_out_tmp <- readRDS(paste0(ct_path, id_name_tmp, '_ct_results.rds'))
   
   ct_out_tmp <- merge(ct_out_tmp, subset_ids,
                       by = "scen_id")
   
+  ## census tract (health sensitivity, county means)
+  ct_hs_out_tmp <- readRDS(paste0(ct_hs_path, id_name_tmp, '_ctc_results.rds'))
+  
+  ct_hs_out_tmp <- merge(ct_hs_out_tmp, subset_ids,
+                      by = "scen_id")
+  
+  ## county out
   county_out_tmp <- readRDS(paste0(county_path, id_name_tmp, '_county_results.rds'))
   
   county_out_tmp <- merge(county_out_tmp, subset_ids,
                       by = "scen_id")
   
+  ## state out (main results)
   state_out_tmp <- readRDS(paste0(state_path, id_name_tmp, '_state_results.rds'))
   
   state_out_tmp <- merge(state_out_tmp, subset_ids,
                           by = "scen_id")
   
-  ct_out_list[[i]]     <- ct_out_tmp
-  county_out_list[[i]] <- county_out_tmp
-  state_out_list[[i]]  <- state_out_tmp
+  ## state out (main results)
+  state_hs_out_tmp <- readRDS(paste0(state_hs_path, id_name_tmp, '_state_results_health.rds'))
+  
+  state_hs_out_tmp <- merge(state_hs_out_tmp, subset_ids,
+                         by = "scen_id")
+  
+  
+  ct_out_list[[i]]        <- ct_out_tmp
+  ct_hs_out_list[[i]]     <- ct_hs_out_tmp
+  county_out_list[[i]]    <- county_out_tmp
+  state_out_list[[i]]     <- state_out_tmp
+  state_hs_out_list[[i]]  <- state_hs_out_tmp
   
 }
 
-ct_subset_all <- rbindlist(ct_out_list)
-county_subset_all <- rbindlist(county_out_list)
-state_subset_all <- rbindlist(state_out_list)
+ct_subset_all        <- rbindlist(ct_out_list)
+ct_hs_subset_all     <- rbindlist(ct_hs_out_list)
+county_subset_all    <- rbindlist(county_out_list)
+state_subset_all     <- rbindlist(state_out_list)
+state_hs_subset_all  <- rbindlist(state_hs_out_list)
 
 fwrite(ct_subset_all, paste0(ct_path, "subset_census_tract_results.csv"))
+fwrite(ct_hs_subset_all, paste0(ct_hs_path, "subset_census_tract_hs_results.csv"))
 fwrite(county_subset_all, paste0(county_path, "subset_county_results.csv"))
 fwrite(state_subset_all, paste0(state_path, "subset_state_results.csv"))
-
+fwrite(state_hs_subset_all, paste0(state_hs_path, "subset_state_hs_results.csv"))
 
