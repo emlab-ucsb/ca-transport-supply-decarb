8d773e6aed2e1be215e9cde577af0a0f16e40a40
Vincent Thivierge <vthivierge@ucsb.edu>
2023-04-26 16:01:14 -0400

    Edits to ref EFs

--- health/scripts/refining_emission_factors.R
+++ health/scripts/refining_emission_factors.R
@@ -1,8 +1,7 @@
 # CalEPA: Homemade refining emission factors from NEI and XXX data
 # vthivierge@ucsb.edu
 # created: 04/05/2023
-# updated: 04/12/2023
-
+# updated: 04/26/2023
 
 # set up environment
 
@@ -45,7 +44,23 @@ nei_2017_raw <- fread(paste0(wd, "/raw/NEI/emis_sum_fac_15420.csv", sep=""))%>%
   #filter(pollutant_code %in% poll_sub_ghg)%>%
   filter(pollutant_type_s %in% "CAP")
 
-# Load production/capactiy/cluster refining data
+## 2014 NEI
+
+nei_2014_raw <- fread(paste0(wd, "/raw/NEI/2014v2facilities.csv", sep=""))%>%
+  clean_names()
+
+nei_2014 <- nei_2014_raw %>%
+  filter(st_usps_cd %in% "CA")%>%
+  mutate(naics6 = str_sub(naics_cd,1,6),
+         reporting_year = 2014)%>%
+  filter(naics6 %in% c("324110"))%>%
+  filter(pollutant_cd %in% c("NOX","SO2","NH3","PM25-PRI","VOC"))%>% 
+  select(eis_facility_site_id,reporting_year,naics6,facility_site_name,county_name,total_emissions,pollutant_cd)%>%
+  distinct()%>% 
+  filter(eis_facility_site_id %notin% c("15859711","4789411","10296811", "2534511"))%>%
+  rename("eis_facility_id"="eis_facility_site_id","site_name"="facility_site_name","county"="county_name","pollutant_code"="pollutant_cd")
+  
+# Load refining-level data
 
 ref_analysis <- fread("G:/Shared drives/emlab/projects/current-projects/calepa-cn/data/stocks-flows/processed/refinery_loc_cap_manual.csv")%>%
   clean_names();ref_analysis
@@ -58,36 +73,50 @@ ref_renewable <- read_excel("G:/Shared drives/emlab/projects/current-projects/ca
 ref_alt <- read_excel("G:/Shared drives/emlab/projects/current-projects/calepa-cn/data/stocks-flows/raw/altair_refinery_capacity.xlsx")%>%
   clean_names();ref_alt
 
+# Load cluster level production data
+
+ref_prod <- fread("G:/Shared drives/emlab/projects/current-projects/calepa-cn/data/stocks-flows/processed/fuel_watch_data.csv")%>%
+  clean_names()%>%
+  filter(stock_flow == "Refinery Input")%>%
+  #filter(stock_flow == "Refinery Production")%>%
+  group_by(year,region)%>%
+  summarise(thous_barrels = sum(thous_barrels, na.rm=T))%>%
+  ungroup()%>%
+  filter(year %in% c(2014,2017))
+
 # Process NEI data: restrict to refining & california
 
 nei_ca_ref <- nei_2017_raw %>%
   filter(state %in% "CA")%>%
   mutate(naics6 = str_sub(naics_code,1,6),
          naics4 = str_sub(naics_code,1,4))%>%
-  filter(naics6 %in% c("324110","325193"))%>%
-  filter(pollutant_code %in% c("NOX","SO2","NH3","PM25-PRI","VOC"))
-  #filter(naics4 %in% c("3241","3251"))
-
-#graph
-nei_ca_ref%>%
-  mutate(company_clean = str_remove_all(company_name, c("REFINERY")))%>%
-  mutate(company_clean = str_remove_all(company_clean, c("LLC")))%>%
-  mutate(company_clean = str_remove_all(company_clean, c("COMPANY")))%>%
-  mutate(company_clean = str_remove_all(company_clean, c("REFINING")))%>%
-  mutate(company_clean = str_remove_all(company_clean, c("CO")))%>%
-  #ggplot(aes(x=company_name, y = total_emissions))+
-  ggplot(aes(fct_reorder(company_clean, -total_emissions), y = total_emissions))+
-  geom_col()+
-  facet_wrap(~pollutant_code)+
-  theme_cowplot()+
-  theme(axis.text.x = element_text(angle = 45, hjust=1))+
-  labs(x = "Facility name", y = "2017 emissions (tons)")
-
+  #filter(naics6 %in% c("324110","325193"))%>% #Do not include ethanol refineries
+  filter(naics6 %in% c("324110"))%>%
+  #filter(naics4 %in% c("3241"))%>%
+  filter(pollutant_code %in% c("NOX","SO2","NH3","PM25-PRI","VOC"))%>%
+  select(eis_facility_id,reporting_year,naics6,company_name,site_name,county,total_emissions,pollutant_code)%>%
+  bind_rows(nei_2014)
+  
+# #graph
+# nei_ca_ref%>%
+#   mutate(company_clean = str_remove_all(company_name, c("REFINERY")))%>%
+#   mutate(company_clean = str_remove_all(company_clean, c("LLC")))%>%
+#   mutate(company_clean = str_remove_all(company_clean, c("COMPANY")))%>%
+#   mutate(company_clean = str_remove_all(company_clean, c("REFINING")))%>%
+#   mutate(company_clean = str_remove_all(company_clean, c("CO")))%>%
+#   #ggplot(aes(x=company_name, y = total_emissions))+
+#   ggplot(aes(fct_reorder(company_clean, -total_emissions), y = total_emissions))+
+#   geom_col()+
+#   facet_wrap(~pollutant_code)+
+#   theme_cowplot()+
+#   theme(axis.text.x = element_text(angle = 45, hjust=1))+
+#   labs(x = "Facility name", y = "2017 emissions (tons)")
 
 # Match refineries
 
 ref_analysis %>% select(site_id,company,corp,refinery_name,county) %>% arrange(county)
-nei_ca_ref %>% select(eis_facility_id,company_name,site_name,county) %>% distinct() %>% arrange(county)
+nei_ca_ref %>% select(eis_facility_id,naics6,company_name,site_name,county,total_emissions,pollutant_code) %>% filter(pollutant_code %in% "NOX") %>% distinct() %>% arrange(county)
+nei_ca_ref %>% select(eis_facility_id,naics6,company_name,site_name,county) %>% distinct() %>% arrange(county)
 
 # At the cluster level
 
@@ -105,7 +134,10 @@ cluster_factors <- nei_ca_ref %>%
   summarise(total_emissions = sum(total_emissions, na.rm=TRUE))%>%
   ungroup()%>%
   left_join(cluster_cap_2019, by = c("cluster"))%>%
-  mutate(bbl_year = barrels_per_day*365,
+  left_join(ref_prod, by = c("cluster"="region", "reporting_year"="year"))%>%
+  mutate(prod_per_day = (thous_barrels*1000)/365,
+         capacity_util = prod_per_day/barrels_per_day)%>%
+  mutate(bbl_year = thous_barrels*1000,
          emission_kg = total_emissions*1000, #Ton to kg
          kg_bbl = emission_kg/bbl_year)
 
@@ -133,12 +165,24 @@ cluster_factors %>%
   ggplot(aes(fct_reorder(pollutant_code, -emission_kg), y = kg_bbl))+
   geom_col()+
   geom_col(data = JM_ref, aes(x=pollutant ,y=quantity_kg_bbl))+
-  facet_wrap(~cluster)+
+  facet_grid(reporting_year~cluster)+
   theme_cowplot()+
   theme(axis.text.x = element_text(angle = 45, hjust=1))+
   labs(x = "Criteria pollutant", y = "Emission factor (kg/bbl)",
        title = "Figure 1: Refining emission factors")
 
+cluster_factors %>%
+  ggplot(aes(x=reporting_year, y = kg_bbl, group=cluster,color=cluster))+
+  geom_line()+
+  geom_point()+
+  facet_wrap(~pollutant_code)+
+  theme_cowplot()+
+  labs(x = "Year", y = "Emission factor (kg/bbl)",
+       title = "Figure 2: Refining emission factors by year",
+       color = "Cluster")+
+  scale_x_continuous(breaks = c(2014,2017))
+  
+  
 # ## Read raw emission factors from Jaramillo & Muller (2016)
 # 
 # ef_raw <- read.csv("./emission_factors.csv", stringsAsFactors = FALSE)%>%
